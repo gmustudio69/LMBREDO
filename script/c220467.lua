3842222f40406928548f077fff4c0da41334c5a5
Nguyễn Vương Văn Khiêm <nguyenvuongvankhiem1132@gmail.com>
2026-03-24 20:28:57 +0700

    Feat : Add image rikka Nym and Rikka Slyphid card

--- script/c220467.lua
+++ script/c220467.lua
@@ -1,5 +1,159 @@
 --Rikka Sylphid
-local s,id,o=GetID()
+local s,id=GetID()
 function s.initial_effect(c)
+	-- Xyz Summon (2 Level 4 Plant monsters)
+	Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsRace,RACE_PLANT),4,2)
+	c:EnableReviveLimit()
+
+	-- Hiệu ứng 1: Quick Effect (Negate Monster hoặc S/T)
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DISABLE)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCountLimit(1,id)
+	e1:SetCost(aux.dxmcost)
+	e1:SetTarget(s.negtg)
+	e1:SetOperation(s.negop)
+	c:RegisterEffect(e1,false,REGISTER_FLAG_DETACH_XMAT)
+
+	-- Hiệu ứng 2: Tái chế từ Mộ khi có monster bị Tribute
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_RELEASE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,id+1)
+	e2:SetCondition(s.thcon)
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
+	c:RegisterEffect(e2)
+end
+
+-- Khai báo Setcode 0x141 cho tộc Rikka
+s.listed_series={0x141}
+s.listed_names={id}
+
+----------------------------------------------------
+-- Logic Hiệu ứng 1 (Negate)
+----------------------------------------------------
+-- Check xem có quái Rikka Xyz khác trên sân không
+function s.altfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0x141) and c:IsType(TYPE_XYZ)
+end
+
+-- Check điều kiện mở rộng để target Spell/Trap
+function s.altcond(hc,tp)
+	local b1=Duel.IsExistingMatchingCard(s.altfilter,tp,LOCATION_MZONE,0,1,hc)
+	local b2=hc:GetOverlayGroup():IsExists(function(mc) return mc:IsSetCard(0x141) and mc:IsType(TYPE_MONSTER) end, 1, nil)
+	return b1 or b2
+end
+
+function s.negfilter(c,hc,tp)
+	if not c:IsFaceup() or c:IsDisabled() then return false end
+	if c:IsType(TYPE_MONSTER) then return true end
+	if c:IsType(TYPE_SPELL+TYPE_TRAP) then
+		return s.altcond(hc,tp)
+	end
+	return false
+end
+
+function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local c=e:GetHandler()
+	if chkc then return chkc:IsControler(1-tp) and chkc:IsOnField() and s.negfilter(chkc,c,tp) end
+	if chk==0 then return Duel.IsExistingTarget(s.negfilter,tp,0,LOCATION_ONFIELD,1,nil,c,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_NEGATE)
+	local g=Duel.SelectTarget(tp,s.negfilter,tp,0,LOCATION_ONFIELD,1,1,nil,c,tp)
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,g,1,0,0)
+end
+
+function s.negop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsFaceup() and tc:IsRelateToEffect(e) and not tc:IsDisabled() then
+		Duel.NegateRelatedChain(tc,RESET_TURN_SET)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetCode(EFFECT_DISABLE)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e2:SetCode(EFFECT_DISABLE_EFFECT)
+		e2:SetValue(RESET_TURN_SET)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e2)
+		if tc:IsType(TYPE_TRAPMONSTER) then
+			local e3=Effect.CreateEffect(c)
+			e3:SetType(EFFECT_TYPE_SINGLE)
+			e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+			e3:SetCode(EFFECT_DISABLE_TRAPMONSTER)
+			e3:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+			tc:RegisterEffect(e3)
+		end
+	end
+end
+
+----------------------------------------------------
+-- Logic Hiệu ứng 2 (Recycle / Attach)
+----------------------------------------------------
+function s.cfilter(c)
+	-- Kiểm tra xem lá bài bị tribute có phải là quái thú không (hoặc nó đã từng là quái thú trên sân)
+	return c:IsType(TYPE_MONSTER) or c:IsPreviousLocation(LOCATION_MZONE)
+end
+
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.cfilter,1,nil)
+end
+
+function s.xyzfilter(c)
+	return c:IsFaceup() and c:IsType(TYPE_XYZ) and c:IsRace(RACE_PLANT)
+end
+
+function s.thfilter(c,tp)
+	if not (c:IsSetCard(0x141) and not c:IsCode(id)) then return false end
+	return c:IsAbleToHand() or Duel.IsExistingMatchingCard(s.xyzfilter,tp,LOCATION_MZONE,0,1,nil)
+end
+
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.thfilter(chkc,tp) end
+	if chk==0 then return Duel.IsExistingTarget(s.thfilter,tp,LOCATION_GRAVE,0,1,nil,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	local g=Duel.SelectTarget(tp,s.thfilter,tp,LOCATION_GRAVE,0,1,1,nil,tp)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_TOHAND,g,1,tp,LOCATION_GRAVE)
+end
+
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if not tc:IsRelateToEffect(e) then return end
+	local b1=tc:IsAbleToHand()
+	local b2=Duel.IsExistingMatchingCard(s.xyzfilter,tp,LOCATION_MZONE,0,1,nil)
 	
+	local op=0
+	if b1 and b2 then
+		-- ID chuỗi hiển thị khi cho phép người chơi lựa chọn (2 là Add to Hand, 3 là Attach)
+		op=Duel.SelectOption(tp,aux.Stringid(id,2),aux.Stringid(id,3))
+	elseif b1 then
+		op=0
+	elseif b2 then
+		op=1
+	else
+		return
+	end
+	
+	if op==0 then
+		Duel.SendtoHand(tc,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,tc)
+	else
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+		local g=Duel.SelectMatchingCard(tp,s.xyzfilter,tp,LOCATION_MZONE,0,1,1,nil)
+		if #g>0 then
+			Duel.Overlay(g:GetFirst(),tc)
+		end
+	end
 end
