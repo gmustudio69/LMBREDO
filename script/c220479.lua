7b406ada017974bb6b52a9316d709c7d278d1130
Nguyễn Vương Văn Khiêm <nguyenvuongvankhiem1132@gmail.com>
2026-03-26 16:01:58 +0700

    Update Effect Plant link 3

--- script/c220479.lua
+++ script/c220479.lua
@@ -6,67 +6,112 @@ function s.initial_effect(c)
 	c:EnableReviveLimit()
 
 	-- ===============================================
-	-- Effect 1: Spell/Trap Protection (Passive)
+	-- Effect 1: Trigger on Link Summon (Shuffle & Draw)
 	-- ===============================================
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetTargetRange(LOCATION_ONFIELD,0)
-	e1:SetTarget(s.indtg)
-	e1:SetValue(aux.indoval)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TODECK+CATEGORY_DRAW)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.tdcon)
+	e1:SetTarget(s.tdtg)
+	e1:SetOperation(s.tdop)
 	c:RegisterEffect(e1)
 
 	-- ===============================================
-	-- Effect 2 & 3: Quick Effect (Tribute to activate 1 option)
-	-- Shared Condition, Cost, and Limit
+	-- Effect 2: Spell/Trap Protection (Passive)
+	-- ===============================================
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTargetRange(LOCATION_ONFIELD,0)
+	e2:SetTarget(s.indtg)
+	e2:SetValue(aux.indoval)
+	c:RegisterEffect(e2)
+
+	-- ===============================================
+	-- Effect 3 & 4: Quick Effect (Tribute to activate 1 option)
 	-- ===============================================
 	
 	-- ● Option 1: Banish from GY
-	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,0))
-	e2:SetCategory(CATEGORY_REMOVE)
-	e2:SetType(EFFECT_TYPE_QUICK_O)
-	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e2:SetCode(EVENT_FREE_CHAIN)
-	e2:SetRange(LOCATION_MZONE)
-	e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_MAIN_END)
-	e2:SetCountLimit(1,id) -- Chung ID HOPT
-	e2:SetCondition(s.optcon)
-	e2:SetCost(s.optcost)
-	e2:SetTarget(s.rmtg)
-	e2:SetOperation(s.rmop)
-	c:RegisterEffect(e2)
-
-	-- ● Option 2: Change to face-down Defense
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,1))
-	e3:SetCategory(CATEGORY_POSITION)
+	e3:SetCategory(CATEGORY_REMOVE)
 	e3:SetType(EFFECT_TYPE_QUICK_O)
 	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e3:SetCode(EVENT_FREE_CHAIN)
 	e3:SetRange(LOCATION_MZONE)
 	e3:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_MAIN_END)
-	e3:SetCountLimit(1,id) -- Chung ID HOPT
+	e3:SetCountLimit(1,id+1) -- Chung ID HOPT với e4
 	e3:SetCondition(s.optcon)
 	e3:SetCost(s.optcost)
-	e3:SetTarget(s.postg)
-	e3:SetOperation(s.posop)
+	e3:SetTarget(s.rmtg)
+	e3:SetOperation(s.rmop)
 	c:RegisterEffect(e3)
+
+	-- ● Option 2: Change to face-down Defense
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,2))
+	e4:SetCategory(CATEGORY_POSITION)
+	e4:SetType(EFFECT_TYPE_QUICK_O)
+	e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e4:SetCode(EVENT_FREE_CHAIN)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_MAIN_END)
+	e4:SetCountLimit(1,id+1) -- Chung ID HOPT với e3
+	e4:SetCondition(s.optcon)
+	e4:SetCost(s.optcost)
+	e4:SetTarget(s.postg)
+	e4:SetOperation(s.posop)
+	c:RegisterEffect(e4)
 end
 
 -- ===============================================
--- Logic Effect 1 (Protection)
+-- Logic Effect 1 (Shuffle & Draw)
+-- ===============================================
+function s.tdcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK)
+end
+function s.tdfilter(c)
+	-- Lấy Plant ở GY hoặc Banish (nếu Banish thì phải Face-up)
+	return c:IsRace(RACE_PLANT) and c:IsAbleToDeck() 
+		and (c:IsLocation(LOCATION_GRAVE) or c:IsFaceup())
+end
+function s.tdtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE+LOCATION_REMOVED) and s.tdfilter(chkc) end
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) 
+		and Duel.IsExistingTarget(s.tdfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g=Duel.SelectTarget(tp,s.tdfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,3,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,#g,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+function s.tdop(e,tp,eg,ep,ev,re,r,rp)
+	local tg=Duel.GetTargetCards(e)
+	if #tg>0 then
+		Duel.SendtoDeck(tg,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
+		local og=Duel.GetOperatedGroup()
+		-- Kiểm tra xem có lá bài nào thực sự được trả về Deck/Extra Deck không
+		if og:IsExists(Card.IsLocation,1,nil,LOCATION_DECK+LOCATION_EXTRA) then
+			Duel.Draw(tp,1,REASON_EFFECT)
+		end
+	end
+end
+
+-- ===============================================
+-- Logic Effect 2 (Protection)
 -- ===============================================
 function s.indtg(e,c)
 	return c:IsFaceup() and (c:IsType(TYPE_SPELL) or c:IsType(TYPE_TRAP))
 end
 
 -- ===============================================
--- Logic Chung cho Effect 2 & 3 (Condition & Cost)
+-- Logic Chung cho Effect 3 & 4 (Condition & Cost)
 -- ===============================================
 function s.optcon(e,tp,eg,ep,ev,re,r,rp)
-	-- Chỉ kích hoạt trong Main Phase
 	local ph=Duel.GetCurrentPhase()
 	return ph==PHASE_MAIN1 or ph==PHASE_MAIN2
 end
