8979ebe41ff28bb7ea9960c3df3626f6355baffc
Nguyễn Vương Văn Khiêm <nguyenvuongvankhiem1132@gmail.com>
2026-03-26 22:26:43 +0700

    Re Effect Nymph

--- script/c220464.lua
+++ script/c220464.lua
@@ -2,7 +2,7 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	-- ===============================================
-	-- Hiệu ứng 1: Tự nhảy từ tay nếu có Rikka
+	-- Hiệu ứng 1: Tự nhảy từ tay nếu có PLANT (Generic)
 	-- ===============================================
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
@@ -14,9 +14,9 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 
 	-- ===============================================
-	-- Hiệu ứng 2: Bị Tribute hoặc Detach -> Gọi quái & Đổi Level
+	-- Hiệu ứng 2: Bị Tribute / Link Material / Xyz Detach -> Hồi sinh
 	-- ===============================================
-	-- Bắt sự kiện bị Tribute
+	-- 2.1: Bắt sự kiện bị Tribute
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
@@ -28,18 +28,22 @@ function s.initial_effect(c)
 	e2:SetOperation(s.revop)
 	c:RegisterEffect(e2)
 	
-	-- Bắt sự kiện bị Detach (gửi xuống mộ làm cost cho Plant Xyz)
+	-- 2.2: Bắt sự kiện bị Detach làm Cost cho Xyz Plant
 	local e3=e2:Clone()
 	e3:SetCode(EVENT_TO_GRAVE)
 	e3:SetCondition(s.detachcon)
 	c:RegisterEffect(e3)
-end
-s.listed_series={0x141}
-s.listed_names={id}
 
--- Logic Effect 1 (Nhảy từ tay)
+	-- 2.3: Bắt sự kiện làm Link Material cho Plant
+	local e4=e2:Clone()
+	e4:SetCode(EVENT_BE_MATERIAL)
+	e4:SetCondition(s.linkcon)
+	c:RegisterEffect(e4)
+end
+
+-- Lọc Quái thú Plant
 function s.cfilter(c)
-	return c:IsFaceup() and c:IsSetCard(0x141)
+	return c:IsFaceup() and c:IsRace(RACE_PLANT)
 end
 function s.spcon(e,c)
 	if c==nil then return true end
@@ -48,13 +52,23 @@ function s.spcon(e,c)
 		and Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil)
 end
 
--- Logic Effect 2 & 3 (Hồi sinh và đổi level)
+-- ===============================================
+-- Logic cho Effect Hồi Sinh
+-- ===============================================
+-- Condition: Detach Xyz
 function s.detachcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsReason(REASON_COST) and re and re:IsActivated() 
 		and re:IsActiveType(TYPE_XYZ) and re:GetHandler():IsRace(RACE_PLANT)
 end
+-- Condition: Link Material
+function s.linkcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsLocation(LOCATION_GRAVE) and r==REASON_LINK and c:GetReasonCard():IsRace(RACE_PLANT)
+end
+
+-- Thực thi Hồi Sinh
 function s.revfilter(c,e,tp)
-	return c:IsRace(RACE_PLANT) and not c:IsCode(id) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:IsRace(RACE_PLANT) and not c:IsCode(id) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE)
 end
 function s.revtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.revfilter(chkc,e,tp) end
@@ -66,14 +80,28 @@ function s.revtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 end
 function s.revop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) and Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)>0 then
-		-- Cho phép người chơi chọn Level
-		local lvl=Duel.AnnounceNumber(tp,4,6,8)
+	if tc:IsRelateToEffect(e) and Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP_DEFENSE)>0 then
+		-- Cho phép chọn Level 1, 4, 6, hoặc 8
+		local lvl=Duel.AnnounceNumber(tp,1,4,6,8)
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_CHANGE_LEVEL)
 		e1:SetValue(lvl)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 		tc:RegisterEffect(e1)
+
+		-- Khóa Plant Summon
+		local e2=Effect.CreateEffect(e:GetHandler())
+		e2:SetType(EFFECT_TYPE_FIELD)
+		e2:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+		e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
+		e2:SetDescription(aux.Stringid(id,2)) -- "You can only Special Summon Plant monsters"
+		e2:SetTargetRange(1,0)
+		e2:SetTarget(s.splimit)
+		e2:SetReset(RESET_PHASE+PHASE_END)
+		Duel.RegisterEffect(e2,tp)
 	end
+end
+function s.splimit(e,c,sump,sumtype,sumpos,targetp)
+	return not c:IsRace(RACE_PLANT)
 end
--- script/c220466.lua
+++ script/c220466.lua
@@ -1,7 +1,9 @@
 --Rikka Nymph
 local s,id=GetID()
 function s.initial_effect(c)
+	-- ===============================================
 	-- Hiệu ứng 1: Đặc biệt triệu hồi từ tay
+	-- ===============================================
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TODECK)
@@ -14,7 +16,9 @@ function s.initial_effect(c)
 	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
 
-	-- Hiệu ứng 2: Thêm 1 lá Rikka từ Deck vào tay khi Summon (Special)
+	-- ===============================================
+	-- Hiệu ứng 2: Thêm 1 lá Rikka từ Deck vào tay khi Summon
+	-- ===============================================
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
 	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
@@ -30,29 +34,30 @@ function s.initial_effect(c)
 	-- Hiệu ứng 3: TỰ BẢN THÂN bị Tribute (từ Tay hoặc Sân)
 	-- ===============================================
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,2))
+	e3:SetDescription(aux.Stringid(id,2)) -- Bật thông báo Special Summon
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e3:SetProperty(EFFECT_FLAG_DELAY)
 	e3:SetCode(EVENT_RELEASE)
 	e3:SetCountLimit(1,{id,3}) -- Dùng chung Limit với e4
-	e3:SetTarget(s.settg)
-	e3:SetOperation(s.setop)
+	e3:SetTarget(s.sptg2)
+	e3:SetOperation(s.spop2)
 	c:RegisterEffect(e3)
 
 	-- ===============================================
 	-- Hiệu ứng 4: PLANT KHÁC bị Tribute khi lá này đang ở Mộ
 	-- ===============================================
 	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(id,2))
+	e4:SetDescription(aux.Stringid(id,2)) -- Bật thông báo Special Summon
+	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e4:SetProperty(EFFECT_FLAG_DELAY)
 	e4:SetCode(EVENT_RELEASE)
 	e4:SetRange(LOCATION_GRAVE)
 	e4:SetCountLimit(1,{id,3}) -- Dùng chung Limit với e3
 	e4:SetCondition(s.gycon)
-	e4:SetCost(aux.bfgcost) -- Banish lá bài này từ dưới Mộ làm Cost
-	e4:SetTarget(s.settg)
-	e4:SetOperation(s.setop)
+	e4:SetTarget(s.sptg2)
+	e4:SetOperation(s.spop2)
 	c:RegisterEffect(e4)
 end
 
@@ -106,7 +111,7 @@ function s.thop(e,tp,eg,ep,ev,re,r,rp)
 end
 
 -- ===============================================
--- Logic Hiệu ứng 3 & 4 (Set S/T khi Tribute)
+-- Logic Hiệu ứng 3 & 4 (Special Summon từ Mộ khi có Tribute)
 -- ===============================================
 function s.gyfilter(c,tc)
 	return c:IsRace(RACE_PLANT) and c~=tc
@@ -115,16 +120,23 @@ function s.gycon(e,tp,eg,ep,ev,re,r,rp)
 	-- Điều kiện khi ở Mộ: Có ít nhất 1 Plant khác bị Tribute
 	return eg:IsExists(s.gyfilter,1,nil,e:GetHandler())
 end
-function s.setfilter(c)
-	return c:IsSetCard(0x141) and (c:IsType(TYPE_FIELD) or c:IsType(TYPE_TRAP)) and c:IsSSetable()
+function s.sptg2(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
 end
-function s.settg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.setfilter,tp,LOCATION_DECK,0,1,nil) end
-end
-function s.setop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
-	local g=Duel.SelectMatchingCard(tp,s.setfilter,tp,LOCATION_DECK,0,1,1,nil)
-	if #g>0 then
-		Duel.SSet(tp,g:GetFirst())
+function s.spop2(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 then
+		-- Cài đặt Banish khi rời sân
+		local e1=Effect.CreateEffect(c)
+		e1:SetDescription(3300) -- Gắn nhãn "Banish when leaves the field"
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CLIENT_HINT)
+		e1:SetCode(EFFECT_LEAVE_FIELD_REDIRECT)
+		e1:SetValue(LOCATION_REMOVED)
+		e1:SetReset(RESET_EVENT+RESETS_REDIRECT)
+		c:RegisterEffect(e1,true)
 	end
 end
