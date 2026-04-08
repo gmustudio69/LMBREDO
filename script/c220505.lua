c49f528308c448a58990b5a6d68750c32deb94cb
Nebby <65876365+gmustudio69@users.noreply.github.com>
2026-04-08 21:40:26 +0700

    Update c220505.lua

--- script/c220505.lua
+++ script/c220505.lua
@@ -1,124 +1,135 @@
---<Limit Breaker> Fairy Mirage
-local s,id=GetID()
+-- <Limit Breaker> Fairy Mirage
+local s, id = GetID()
+local KAZARI_ID = 220450 -- Replace with the actual ID of "<Limit Breaker> Kazari"
+
 function s.initial_effect(c)
-	--Link Summon
+	-- Must be properly summoned
 	c:EnableReviveLimit()
-	Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsType,TYPE_EFFECT),2,2)
-	--===== SUMMON RESTRICT =====
-	local e0=Effect.CreateEffect(c)
-	e0:SetType(EFFECT_TYPE_SINGLE)
-	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e0:SetCode(EFFECT_SPSUMMON_CONDITION)
-	e0:SetValue(s.splimit)
-	c:RegisterEffect(e0)
-	--===== BECOME KAZARI =====
-	local e1=Effect.CreateEffect(c)
+	-- Link Summon Procedure: 2 monsters with the same Attribute
+	Link.AddProcedure(c, nil, 2, 2, s.lcheck)
+	
+	-- Mentions Kazari (necessary for EDOPro to recognize "ListsCode" effects)
+	aux.AddCodeList(c, KAZARI_ID)
+
+	-- Cannot Special Summon this card, unless you control Kazari or a monster that mentions it
+	local e1 = Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetCode(EFFECT_CHANGE_CODE)
-	e1:SetValue(220450)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e1:SetValue(s.splimit)
 	c:RegisterEffect(e1)
 
-	--===== REVIVE FROM SPELL =====
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e2:SetCode(EVENT_PHASE+PHASE_STANDBY)
-	e2:SetRange(LOCATION_SZONE)
-	e2:SetCountLimit(1,id)
-	e2:SetCondition(s.spellcon)
-	e2:SetTarget(s.spelltg)
-	e2:SetOperation(s.spellop)
+	-- This card becomes "<Limit Breaker> Kazari" while on the field
+	local e2 = Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetCode(EFFECT_CHANGE_CODE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetValue(KAZARI_ID)
 	c:RegisterEffect(e2)
-	--===== QUICK EFFECT =====
-	local e3=Effect.CreateEffect(c)
-	e3:SetCategory(CATEGORY_TOHAND)
-	e3:SetType(EFFECT_TYPE_QUICK_O)
-	e3:SetCode(EVENT_FREE_CHAIN)
-	e3:SetRange(LOCATION_MZONE)
-	e3:SetCountLimit(1,id+1)
-	e3:SetTarget(s.qctg)
-	e3:SetOperation(s.qcop)
+
+	-- Standby Phase: If this card is a Continuous Spell: You can Special Summon this card
+	local e3 = Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id, 0))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_PHASE + PHASE_STANDBY)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetCondition(s.spcon)
+	e3:SetTarget(s.sptg)
+	e3:SetOperation(s.spop)
 	c:RegisterEffect(e3)
 
+	-- Quick Effect: Target up to 2 monsters you control/GY; place them as Cont. Spells, then bounce cards
+	local e4 = Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id, 1))
+	e4:SetCategory(CATEGORY_TOHAND)
+	e4:SetType(EFFECT_TYPE_QUICK_O)
+	e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e4:SetCode(EVENT_FREE_CHAIN)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCountLimit(1, id) -- HOPT limit
+	e4:SetTarget(s.thtg)
+	e4:SetOperation(s.thop)
+	c:RegisterEffect(e4)
 end
 s.listed_names={220450,id}
---===== LINK CHECK (same attribute) =====
-function s.lcheck(g,lc,sumtype,tp)
-	return g:GetClassCount(Card.GetAttribute)==1
+-- Link Summon check: 2 monsters with the exact same Attribute
+function s.lcheck(g, lc, sumtype, tp)
+	return g:GetClassCount(Card.GetAttribute) == 1
 end
 
---===== SUMMON LIMIT =====
-function s.kzfilter(c)
-	return c:IsFaceup() and (c:IsCode(220450) or c:ListsCode(220450))
+-- Special Summon restriction check
+function s.cfilter(c)
+	return c:IsFaceup() and (c:IsCode(KAZARI_ID) or c:ListsCode(KAZARI_ID))
+end
+function s.splimit(e, se, sp, st)
+	return Duel.IsExistingMatchingCard(s.cfilter, e:GetHandlerPlayer(), LOCATION_MZONE, 0, 1, nil)
 end
 
-function s.splimit(e,se,sp,st)
-	return Duel.IsExistingMatchingCard(s.kzfilter,sp,LOCATION_MZONE,0,1,nil)
+-- Standby Phase Special Summon Condition (Must be a Continuous Spell)
+function s.spcon(e, tp, eg, ep, ev, re, r, rp)
+	local c = e:GetHandler()
+	return c:IsType(TYPE_SPELL) and c:IsType(TYPE_CONTINUOUS)
 end
-
---===== SPELL REVIVE =====
-function s.spellcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():IsType(TYPE_SPELL)
+function s.sptg(e, tp, eg, ep, ev, re, r, rp, chk)
+	if chk == 0 then return Duel.GetLocationCount(tp, LOCATION_MZONE) > 0
+		and e:GetHandler():IsCanBeSpecialSummoned(e, 0, tp, false, false) end
+	Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, e:GetHandler(), 1, 0, 0)
 end
-
-function s.spelltg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end
-end
-
-function s.spellop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if not c:IsRelateToEffect(e) then return end
-	Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
-end
-
---===== TARGET =====
-function s.plfilter(c)
-	return c:IsMonster() and (c:IsControler(tp) or c:IsLocation(LOCATION_GRAVE))
-end
-
-function s.qctg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local g1=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_MZONE+LOCATION_GRAVE,0,nil)
-	if chk==0 then
-	return Duel.GetLocationCount(tp,LOCATION_SZONE)>=1
-	and #g1>0
+function s.spop(e, tp, eg, ep, ev, re, r, rp)
+	local c = e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.SpecialSummon(c, 0, tp, tp, false, false, POS_FACEUP)
 	end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,1-tp,LOCATION_ONFIELD)
 end
 
---===== OPERATION =====
-function s.qcop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 then return end
+-- Filter for placing monsters into the Spell/Trap zone
+function s.pcfilter(c)
+	return c:IsMonster() and ((c:IsLocation(LOCATION_MZONE) and c:IsFaceup()) or c:IsLocation(LOCATION_GRAVE)) and not c:IsForbidden()
+end
 
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
-	local g=Duel.SelectMatchingCard(tp,Card.IsMonster,tp,LOCATION_MZONE+LOCATION_GRAVE,0,1,2,nil)
+-- Quick Effect Target
+function s.thtg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE + LOCATION_GRAVE) and chkc:IsControler(tp) and s.pcfilter(chkc) end
+	if chk == 0 then return Duel.GetLocationCount(tp, LOCATION_SZONE) > 0
+		and Duel.IsExistingTarget(s.pcfilter, tp, LOCATION_MZONE + LOCATION_GRAVE, 0, 1, nil) end
+	local ft = Duel.GetLocationCount(tp, LOCATION_SZONE)
+	local max = math.min(2, ft)
+	Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_TOFIELD)
+	local g = Duel.SelectTarget(tp, s.pcfilter, tp, LOCATION_MZONE + LOCATION_GRAVE, 0, 1, max, nil)
+	Duel.SetPossibleOperationInfo(0, CATEGORY_TOHAND, nil, 1, 1 - tp, LOCATION_ONFIELD)
+end
 
-	local ct=0
-	for tc in aux.Next(g) do
-	if tc:IsRelateToEffect(e) then
-
-	--move to SZONE
-	if Duel.MoveToField(tc,tp,tp,LOCATION_SZONE,POS_FACEUP,true) then
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_CHANGE_TYPE)
-	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e1:SetValue(TYPE_SPELL+TYPE_CONTINUOUS)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-	tc:RegisterEffect(e1)
-	ct=ct+1
+-- Quick Effect Operation
+function s.thop(e, tp, eg, ep, ev, re, r, rp)
+	local tg = Duel.GetTargetCards(e)
+	if #tg == 0 then return end
+	local ft = Duel.GetLocationCount(tp, LOCATION_SZONE)
+	if ft < #tg then return end -- Cannot resolve if zones are suddenly blocked
+	
+	local placed = 0
+	for tc in aux.Next(tg) do
+		if Duel.MoveToField(tc, tp, tc:GetOwner(), LOCATION_SZONE, POS_FACEUP, true) then
+			-- Treat as Continuous Spell
+			local e1 = Effect.CreateEffect(e:GetHandler())
+			e1:SetCode(EFFECT_CHANGE_TYPE)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+			e1:SetReset(RESET_EVENT + RESETS_STANDARD - RESET_TURN_SET)
+			e1:SetValue(TYPE_SPELL + TYPE_CONTINUOUS)
+			tc:RegisterEffect(e1)
+			placed = placed + 1
+		end
 	end
-
+	
+	-- Optional Bounce Effect based on how many cards were placed
+	if placed > 0 then
+		local g = Duel.GetMatchingGroup(Card.IsFaceup, tp, 0, LOCATION_ONFIELD, nil)
+		if #g > 0 and Duel.SelectYesNo(tp, aux.Stringid(id, 2)) then -- Prompts: "Do you want to return opponent's cards to hand?"
+			Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_RTOHAND)
+			local sg = g:Select(tp, 1, placed, nil)
+			Duel.SendtoHand(sg, nil, REASON_EFFECT)
+		end
 	end
-	end
-
-	if ct>0 then
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
-	local g2=Duel.SelectMatchingCard(tp,Card.IsFaceup,tp,0,LOCATION_ONFIELD,1,ct,nil)
-	if #g2>0 then
-	Duel.SendtoHand(g2,nil,REASON_EFFECT)
-	end
-	end
-
 end
