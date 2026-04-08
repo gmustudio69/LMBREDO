10689d918d88d4923f672292a3ae7a88123aa53c
Nebby <65876365+gmustudio69@users.noreply.github.com>
2026-04-08 21:35:11 +0700

    Update c220505.lua

--- script/c220505.lua
+++ script/c220505.lua
@@ -4,7 +4,13 @@ function s.initial_effect(c)
 	--Link Summon
 	c:EnableReviveLimit()
 	Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsType,TYPE_EFFECT),2,2)
-
+	--===== SUMMON RESTRICT =====
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e0:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e0:SetValue(s.splimit)
+	c:RegisterEffect(e0)
 	--===== BECOME KAZARI =====
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
