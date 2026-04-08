a110f0520e0f9293778b66f8f49da5b3c1e287d7
Nebby <65876365+gmustudio69@users.noreply.github.com>
2026-04-08 13:51:52 +0700

    fix kazari

--- script/c220450.lua
+++ script/c220450.lua
@@ -2,7 +2,6 @@
 local s,id=GetID()
 
 function s.initial_effect(c)
-
 	--------------------------------------------------
 	-- Special Summon from hand
 	--------------------------------------------------
@@ -22,108 +21,79 @@ function s.initial_effect(c)
 	--------------------------------------------------
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_EQUIP)
 	e2:SetType(EFFECT_TYPE_QUICK_O)
 	e2:SetCode(EVENT_FREE_CHAIN)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e2:SetCountLimit(1,{id,1})
-	e2:SetTarget(s.eqtg)
-	e2:SetOperation(s.eqop)
+	e2:SetCondition(s.btplcon)
+	e2:SetOperation(s.btplop)
 	c:RegisterEffect(e2)
-
-	--------------------------------------------------
-	-- Lose ATK → revive + token
-	--------------------------------------------------
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(id,2))
-	e3:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e3:SetType(EFFECT_TYPE_IGNITION)
-	e3:SetRange(LOCATION_MZONE)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetRange(LOCATION_SZONE)
 	e3:SetCountLimit(1,{id,2})
-	e3:SetCondition(s.atkcon)
-	e3:SetTarget(s.atktg)
-	e3:SetOperation(s.atkop)
+	e3:SetCondition(function(e) return e:GetHandler():IsContinuousSpell() end)
+	e3:SetTarget(s.gypltg)
+	e3:SetOperation(s.gyplop)
 	c:RegisterEffect(e3)
 end
 
 --------------------------------------------------
 -- SS condition
 --------------------------------------------------
-function s.spcon(e,c)
-	if c==nil then return true end
-	return Duel.GetFieldGroupCount(c:GetControler(),0,LOCATION_MZONE)>0
-		and Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
+function s.checkzones(c0,c1)
+	local p0,p1=c0:GetOwner(),c1:GetOwner()
+	if p0==p1 then return Duel.GetLocationCount(p0,LOCATION_SZONE)>1 end
+	return Duel.GetLocationCount(p0,LOCATION_SZONE)>0 and Duel.GetLocationCount(p1,LOCATION_SZONE)>0
 end
-
---------------------------------------------------
--- Equip
---------------------------------------------------
-function s.eqfilter(c)
-	return c:IsFaceup() and not c:IsImmuneToEffect(nil)
+function s.btplcon(e,tp,eg,ep,ev,re,r,rp)
+	local bc0,bc1=Duel.GetBattleMonster(tp)
+	return bc0 and bc1 and bc0==e:GetHandler() and s.checkzones(bc0,bc1)
 end
-
-function s.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsOnField() and s.eqfilter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.eqfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
-	Duel.SelectTarget(tp,s.eqfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+function s.stplace(c,tp,rc)
+	if not Duel.MoveToField(c,tp,c:GetOwner(),LOCATION_SZONE,POS_FACEUP,c:IsMonsterCard()) then return end
+	--Treated as a Continuous Spell
+	local e1=Effect.CreateEffect(rc)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetCode(EFFECT_CHANGE_TYPE)
+	e1:SetValue(TYPE_SPELL|TYPE_CONTINUOUS)
+	e1:SetReset(RESET_EVENT|(RESETS_STANDARD&~RESET_TURN_SET))
+	c:RegisterEffect(e1)
+	return true
 end
-
-function s.eqop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local tc=Duel.GetFirstTarget()
-	if not c:IsRelateToEffect(e) or not tc or not tc:IsRelateToEffect(e) then return end
-
-	if Duel.Equip(tp,tc,c,true) then
-		-- Treat as Equip Spell
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_CHANGE_TYPE)
-		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-		e1:SetValue(TYPE_EQUIP+TYPE_SPELL)
-		tc:RegisterEffect(e1)
+function s.btplop(e,tp,eg,ep,ev,re,r,rp)
+	local bc0,bc1=Duel.GetBattleMonster(tp)
+	if bc0 and bc1 and bc0:IsRelateToBattle() and not bc0:IsImmuneToEffect(e) 
+		and bc1:IsRelateToBattle() and not bc1:IsImmuneToEffect(e) 
+		and s.checkzones(bc0,bc1) and s.stplace(bc0,tp,bc0) then
+		s.stplace(bc1,tp,bc0)
 	end
 end
-
---------------------------------------------------
--- Condition: has equip
---------------------------------------------------
-function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():GetEquipGroup():GetCount()>0
+function s.gyplfilter(c)
+	return c:IsSetCard(0xb67) and not c:IsForbidden()
 end
-
-function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-end
-
-function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+function s.gypltg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and s.gyplfilter(chkc) end
 	local c=e:GetHandler()
-	local g=c:GetEquipGroup()
-	if #g==0 then return end
-
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
-	local ec=g:Select(tp,1,1,nil):GetFirst()
-	if not ec then return end
-
-	local atk=ec:GetTextAttack()
-
-	-- Reduce ATK
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_UPDATE_ATTACK)
-	e1:SetValue(-atk)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-	c:RegisterEffect(e1)
-
-	if c:GetAttack()==0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
-		Duel.SpecialSummon(ec,0,tp,tp,true,true,POS_FACEUP)
-
-		-- Token
-		if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
-			local token=Duel.CreateToken(tp,0) -- bạn cần tạo token ID riêng
-			Duel.SpecialSummon(token,0,tp,tp,false,false,POS_FACEUP)
-		end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingTarget(s.gyplfilter,tp,LOCATION_MZONE,0,1,nil)
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	local g=Duel.SelectTarget(tp,s.gyplfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+end
+function s.gyplop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	local c=e:GetHandler()
+	if tc:IsRelateToEffect(e) and not tc:IsImmuneToEffect(e)
+		and s.stplace(tc,tp,c) and c:IsRelateToEffect(e) then
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
