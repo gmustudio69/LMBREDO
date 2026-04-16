bb547dd4993fcae41aee03b4d5d82ca4a2e479ea
Nebby <65876365+gmustudio69@users.noreply.github.com>
2026-04-17 00:34:39 +0700

    Create c220540.lua

---
+++ script/c220540.lua
@@ -0,0 +1,11 @@
+local s,id=GetID()
+function s.initial_effect(c)
+	-- This card's Attribute becomes none (0)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetCode(EFFECT_CHANGE_ATTRIBUTE)
+	e1:SetRange(LOCATION_MZONE+LOCATION_GY)
+	e1:SetValue(0)
+	c:RegisterEffect(e1)
+end
