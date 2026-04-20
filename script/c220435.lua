96e8246137a4fc0ffce9a11bda94f388242a3906
Nebby <65876365+gmustudio69@users.noreply.github.com>
2026-04-20 15:24:29 +0700

    Update c220435.lua

--- script/c220435.lua
+++ script/c220435.lua
@@ -66,12 +66,10 @@ end
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.NegateSummon(eg) then
 		Duel.Destroy(eg,REASON_EFFECT)
-
 		if Duel.IsExistingMatchingCard(s.synfilter,tp,LOCATION_MZONE,0,1,nil)
 		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp)
 		and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
-
 			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 			local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
 			if #g>0 then
