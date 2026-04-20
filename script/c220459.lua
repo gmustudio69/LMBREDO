6d79dd7525e250a9501986e879e0820250e0ad43
Nebby <65876365+gmustudio69@users.noreply.github.com>
2026-04-20 15:15:15 +0700

    Continous Trap

--- script/c220459.lua
+++ script/c220459.lua
@@ -1,109 +1,135 @@
---Limit Break - Finale
 local s,id=GetID()
 function s.initial_effect(c)
+	-- Effect 1: Choose 1 of 2 effects
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_QUICK_O) -- Use EFFECT_TYPE_QUICK_O if this is a Trap Card
+	e1:SetRange(LOCATION_SZONE)
+	e1:SetCountLimit(1)
+	e1:SetTarget(s.e1tg)
+	e1:SetOperation(s.e1op)
+	c:RegisterEffect(e1)
 
---Recycle traps and set
-local e1=Effect.CreateEffect(c)
-e1:SetCategory(CATEGORY_TODECK+CATEGORY_SET)
-e1:SetType(EFFECT_TYPE_QUICK_O)
-e1:SetRange(LOCATION_SZONE)
-e1:SetCountLimit(1)
-e1:SetTarget(s.settg)
-e1:SetOperation(s.setop)
-c:RegisterEffect(e1)
-
---Protection effect
-local e2=Effect.CreateEffect(c)
-e2:SetCategory(CATEGORY_RELEASE)
-e2:SetType(EFFECT_TYPE_QUICK_O)
-e2:SetRange(LOCATION_SZONE)
-e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
-e2:SetCountLimit(1,id+1)
-e2:SetCost(s.protcost)
-e2:SetTarget(s.prottg)
-e2:SetOperation(s.protop)
-c:RegisterEffect(e2)
-
+	-- Effect 2: Protection by Tributing
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,3))
+	e2:SetType(EFFECT_TYPE_QUICK_O) -- Use EFFECT_TYPE_QUICK_O if this is a Trap/Quick Effect
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCountLimit(1,id)
+	e2:SetCost(s.protcost)
+	e2:SetTarget(s.prottg)
+	e2:SetOperation(s.protop)
+	c:RegisterEffect(e2)
 end
 
---Trap recycle filter
+-- ==========================================
+-- Filters for Effect 1
+-- ==========================================
 function s.tdfilter(c)
-return c:IsType(TYPE_TRAP) and c:IsAbleToDeck()
+	return c:IsType(TYPE_TRAP) and c:IsAbleToDeck()
+end
+function s.setfilter(c)
+	return c:IsType(TYPE_TRAP) and c:IsSSetable()
+end
+function s.syncfilter(c)
+	return c:IsFaceup() and c:IsType(TYPE_SYNCHRO) and c:IsLevel(13)
 end
 
-function s.synfilter(c)
-return c:IsFaceup() and c:IsType(TYPE_SYNCHRO) and c:IsLevel(13)
+function s.e1tg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then
+		if e:GetLabel()==0 then
+			return chkc:IsLocation(LOCATION_GY) and chkc:IsControler(tp) and s.tdfilter(chkc)
+		else return false end
+	end
+	local b1=Duel.IsExistingTarget(s.tdfilter,tp,LOCATION_GY,0,2,nil) and Duel.IsPlayerCanDraw(tp,1)
+	local b2=Duel.IsExistingMatchingCard(s.setfilter,tp,LOCATION_HAND,0,1,nil)
+	if chk==0 then return b1 or b2 end
+	
+	local op=0
+	if b1 and b2 then
+		op=Duel.SelectOption(tp,aux.Stringid(id,1),aux.Stringid(id,2))
+	elseif b1 then
+		op=Duel.SelectOption(tp,aux.Stringid(id,1))
+	else
+		op=Duel.SelectOption(tp,aux.Stringid(id,2))+1
+	end
+	e:SetLabel(op)
+	
+	if op==0 then
+		-- Target 2 Traps
+		e:SetCategory(CATEGORY_TODECK+CATEGORY_DRAW)
+		e:SetProperty(EFFECT_FLAG_CARD_TARGET)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+		local g=Duel.SelectTarget(tp,s.tdfilter,tp,LOCATION_GY,0,2,2,nil)
+		Duel.SetOperationInfo(0,CATEGORY_TODECK,g,2,0,0)
+		Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+	else
+		-- Set from Hand (No target needed)
+		e:SetCategory(0)
+		e:SetProperty(0)
+	end
 end
 
-function s.settg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-if chkc then return chkc:IsLocation(LOCATION_GRAVE) and s.tdfilter(chkc) end
-if chk==0 then
-return Duel.IsExistingTarget(s.tdfilter,tp,LOCATION_GRAVE,0,2,nil)
-and Duel.IsExistingMatchingCard(Card.IsSSetable,tp,LOCATION_HAND,0,1,nil)
-end
-Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-local g=Duel.SelectTarget(tp,s.tdfilter,tp,LOCATION_GRAVE,0,2,2,nil)
-Duel.SetOperationInfo(0,CATEGORY_TODECK,g,2,0,0)
-end
-
-function s.setop(e,tp,eg,ep,ev,re,r,rp)
-local c=e:GetHandler()
-local g=Duel.GetTargetCards(e)
-if #g>0 then
-Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
-end
-
-if Duel.IsExistingMatchingCard(Card.IsSSetable,tp,LOCATION_HAND,0,1,nil)
-and Duel.GetLocationCount(tp,LOCATION_SZONE)>0
-and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
-
-Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
-local sg=Duel.SelectMatchingCard(tp,Card.IsSSetable,tp,LOCATION_HAND,0,1,1,nil)
-local tc=sg:GetFirst()
-if tc then
-Duel.SSet(tp,tc)
-
-if Duel.IsExistingMatchingCard(s.synfilter,tp,LOCATION_MZONE,0,1,nil) then
-local e1=Effect.CreateEffect(c)
-e1:SetType(EFFECT_TYPE_SINGLE)
-e1:SetCode(EFFECT_TRAP_ACT_IN_SET_TURN)
-e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-tc:RegisterEffect(e1)
+function s.e1op(e,tp,eg,ep,ev,re,r,rp)
+	local op=e:GetLabel()
+	if op==0 then
+		-- Shuffle and Draw
+		local tg=Duel.GetTargetCards(e)
+		if #tg>0 then
+			Duel.SendtoDeck(tg,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
+			local g=Duel.GetOperatedGroup()
+			if g:IsExists(Card.IsLocation,1,nil,LOCATION_DECK+LOCATION_EXTRA) then
+				Duel.ShuffleDeck(tp)
+				Duel.Draw(tp,1,REASON_EFFECT)
+			end
+		end
+	else
+		-- Set Trap from Hand
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+		local g=Duel.SelectMatchingCard(tp,s.setfilter,tp,LOCATION_HAND,0,1,1,nil)
+		if #g>0 then
+			local tc=g:GetFirst()
+			if Duel.SSet(tp,tc)~=0 then
+				-- Check for Level 13 Synchro
+				if Duel.IsExistingMatchingCard(s.syncfilter,tp,LOCATION_MZONE,0,1,nil) then
+					local e1=Effect.CreateEffect(e:GetHandler())
+					e1:SetType(EFFECT_TYPE_SINGLE)
+					e1:SetCode(EFFECT_TRAP_ACT_IN_SET_TURN)
+					e1:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+					e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+					tc:RegisterEffect(e1)
+				end
+			end
+		end
+	end
 end
 
-end
-end
-end
-
---Tribute DARK monster cost
+-- ==========================================
+-- Filters and Functions for Effect 2
+-- ==========================================
 function s.cfilter(c)
-return c:IsAttribute(ATTRIBUTE_DARK) and c:IsReleasable()
+	return c:IsAttribute(ATTRIBUTE_DARK)
 end
-
 function s.protcost(e,tp,eg,ep,ev,re,r,rp,chk)
-if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil) end
-Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
-local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_MZONE,0,1,1,nil)
-Duel.Release(g,REASON_COST)
+	if chk==0 then return Duel.CheckReleaseGroupCost(tp,s.cfilter,1,false,nil,nil) end
+	local g=Duel.SelectReleaseGroupCost(tp,s.cfilter,1,1,false,nil,nil)
+	Duel.Release(g,REASON_COST)
 end
-
 function s.prottg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-if chkc then return chkc:IsOnField() end
-if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
-Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-local g=Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) end
+	if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	Duel.SelectTarget(tp,aux.TRUE,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
 end
-
 function s.protop(e,tp,eg,ep,ev,re,r,rp)
-local tc=Duel.GetFirstTarget()
-if tc and tc:IsRelateToEffect(e) then
-
-local e1=Effect.CreateEffect(e:GetHandler())
-e1:SetType(EFFECT_TYPE_SINGLE)
-e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
-e1:SetValue(1)
-e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-tc:RegisterEffect(e1)
-
-end
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+		e1:SetValue(1)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+	end
 end
