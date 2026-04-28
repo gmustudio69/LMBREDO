d27ad4336e93367c8ba644bbaf036149f3b541c3
Nebby <65876365+gmustudio69@users.noreply.github.com>
2026-04-28 22:09:54 +0700

    Update c220438.lua

--- script/c220438.lua
+++ script/c220438.lua
@@ -63,7 +63,7 @@ function s.tgop(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetTarget(function(e,c) return c:IsLocation(LOCATION_DECK|LOCATION_EXTRA) and not (c:IsRace(RACE_PSYCHIC) or c:IsRace(RACE_WARRIOR)) end)
 		Duel.RegisterEffect(e1,tp)
 		--Clock Lizard check
-		aux.addTempLizardCheck(c,tp,function(e,c) return not (c:IsRace(RACE_PSYCHIC) or c:IsRace(RACE_WARRIOR)) end)
+		aux.addTempLizardCheck(e:GetHandler(),tp,function(e,e:GetHandler()) return not (c:IsRace(RACE_PSYCHIC) or c:IsRace(RACE_WARRIOR)) end)
 	end
 end
 function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
