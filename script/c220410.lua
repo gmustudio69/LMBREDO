eba6da2d960c0044bbdd99019c02d43557050644
Nebby <65876365+gmustudio69@users.noreply.github.com>
2026-05-01 16:34:37 +0700

    Fix

--- script/c220410.lua
+++ script/c220410.lua
@@ -23,7 +23,7 @@ function s.initial_effect(c)
 	-- Attach banished card
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,0))
-	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
 	e3:SetCode(EVENT_REMOVE)
 	e3:SetRange(LOCATION_MZONE)
 	e3:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
@@ -32,18 +32,18 @@ function s.initial_effect(c)
 	e3:SetOperation(s.mtop)
 	c:RegisterEffect(e3)
 
-	-- Copy "Limit" Spell effect
-	local e5=Effect.CreateEffect(c)
-	e5:SetDescription(aux.Stringid(id,1))
-	e5:SetType(EFFECT_TYPE_QUICK_O)
-	e5:SetCode(EVENT_FREE_CHAIN)
-	e5:SetRange(LOCATION_MZONE)
-	e5:SetCountLimit(1)
-	e5:SetCondition(s.elliecon)
-	e5:SetCost(s.copycost)
-	e5:SetTarget(s.copytg)
-	e5:SetOperation(s.copyop)
-	c:RegisterEffect(e5)
+	-- 3: Quick Effect while controlling Ellie: Copy "Limit" Spell
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,1))
+	e4:SetType(EFFECT_TYPE_QUICK_O)
+	e4:SetCode(EVENT_FREE_CHAIN)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e4:SetCountLimit(1)
+	e4:SetCondition(s.elliecon)
+	e4:SetTarget(s.copytg)
+	e4:SetOperation(s.copyop)
+	c:RegisterEffect(e4)
 end
 
 -- E1: Special Summon Limit
@@ -85,36 +85,55 @@ function s.copycost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,2,REASON_COST) end
 	e:GetHandler():RemoveOverlayCard(tp,2,2,REASON_COST)
 end
-function s.cpfilter(c)
-	return c:IsNormalSpell() and c:IsSetCard(0xf86) and c:IsAbleToRemove()
+function s.copyfilter(c,e,tp,eg,ep,ev,re,r,rp)
+	if not (c:IsSetCard(0xf86) and c:IsType(TYPE_SPELL) and c:IsAbleToRemove()) then return false end
+	-- Ensure the spell has an activatable effect we can copy (bypassing cost)
+	local te=c:CheckActivateEffect(false,true,false)
+	if not te then return false end
+	-- Ensure we have 2 materials to detach later
+	if e:GetHandler():GetOverlayCount()<2 then return false end
+	return true
 end
-function s.copytg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.cpfilter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.cpfilter,tp,LOCATION_GRAVE,0,1,nil) end
+function s.copytg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then
+		local te=e:GetLabelObject()
+		local tg=te and te:GetTarget() or nil
+		return tg and tg(e,tp,eg,ep,ev,re,r,rp,0,chkc)
+	end
+	if chk==0 then return Duel.IsExistingTarget(s.copyfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp,eg,ep,ev,re,r,rp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-	local g=Duel.SelectTarget(tp,s.cpfilter,tp,LOCATION_GRAVE,0,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,1,tp,0)
+	-- Target the Spell first
+	local g=Duel.SelectTarget(tp,s.copyfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp,eg,ep,ev,re,r,rp)
+	local tc=g:GetFirst()
+	local te,ceg,cep,cev,cre,cr,crp=tc:CheckActivateEffect(false,true,true)
+	e:SetLabelObject(te)
+	
+	-- If the copied effect also targets, allow the player to select those targets now
+	local tg=te:GetTarget()
+	if tg then tg(e,tp,ceg,cep,cev,cre,cr,crp,1) end
+	Duel.ClearOperationInfo(0)
 end
 function s.copyop(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetFirstTarget()
-	if not tc:IsRelateToEffect(e) then return end
-	local te,ceg,cep,cev,cre,cr,crp=tc:CheckActivateEffect(false,true,true)
-	if not te then return end
-	local tg=te:GetTarget()
-	local op=te:GetOperation()
-	if tg then tg(te,tp,Group.CreateGroup(),PLAYER_NONE,0,e,REASON_EFFECT,PLAYER_NONE,1) end
-	Duel.BreakEffect()
-	tc:CreateEffectRelation(te)
-	Duel.BreakEffect()
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	
+	-- Find the "Limit" Spell we originally targeted
 	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
-	for etc in aux.Next(g) do
-		etc:CreateEffectRelation(te)
+	if not g then return end
+	local tc=g:Filter(Card.IsType,nil,TYPE_SPELL):GetFirst()
+	
+	-- Banish it and Detach 2
+	if tc and tc:IsRelateToEffect(e) and Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)>0 then
+		if c:CheckRemoveOverlayCard(tp,2,REASON_EFFECT) then
+			Duel.BreakEffect()
+			c:RemoveOverlayCard(tp,2,2,REASON_EFFECT)
+			
+			-- Apply the effect
+			local te=e:GetLabelObject()
+			if te then
+				local op=te:GetOperation()
+				if op then op(e,tp,eg,ep,ev,re,r,rp) end
+			end
+		end
 	end
-	if op then op(te,tp,Group.CreateGroup(),PLAYER_NONE,0,e,REASON_EFFECT,PLAYER_NONE,1) end
-	tc:ReleaseEffectRelation(te)
-	for etc in aux.Next(g) do
-		etc:ReleaseEffectRelation(te)
-	end
-	Duel.BreakEffect()
-	Duel.Remove(te:GetHandler(),POS_FACEUP,REASON_EFFECT)
 end
