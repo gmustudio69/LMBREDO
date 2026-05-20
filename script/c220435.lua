037a26e0e79d5480576096e6754a0fc68cafee61
Nebby <65876365+gmustudio69@users.noreply.github.com>
2026-05-21 00:11:53 +0700

    a

--- script/c220435.lua
+++ script/c220435.lua
@@ -1,29 +1,74 @@
+--Limit Break - The Endless One
 local s,id=GetID()
 function s.initial_effect(c)
-	-- Activate
+	--Counter Trap activation
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
+	e1:SetCategory(CATEGORY_DISABLE_SUMMON+CATEGORY_DESTROY+CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_CHAINING)
+	e1:SetCode(EVENT_SPSUMMON) -- (Lưu ý: Nếu muốn chặn cả Normal/Flip summon thì phải đăng ký thêm event)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
 	e1:SetCondition(s.condition)
 	e1:SetCost(s.cost)
 	e1:SetTarget(s.target)
-	e1:SetOperation(s.activate)
+	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
-	-- Also activate for Summon negation
-	local e2=e1:Clone()
-	e2:SetCode(EVENT_SPSUMMON)
+	--Activate(effect)
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
+	e2:SetType(EFFECT_TYPE_ACTIVATE)
+	e2:SetCode(EVENT_CHAINING)
+	e2:SetCondition(s.condition2)
+	e2:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e2:SetCost(s.cost2)
+	e2:SetTarget(s.target2)
+	e2:SetOperation(s.activate2)
 	c:RegisterEffect(e2)
 end
+--Check summon negatable
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetCurrentChain()==0
+end
 
--- Level 13 Synchro check
-function s.lv13filter(c)
+function s.xyzfilter(c,tp)
+	return c:IsFaceup() and c:IsType(TYPE_XYZ) and c:IsSetCard(0xf86)
+	and c:CheckRemoveOverlayCard(tp,1,REASON_COST)
+end
+
+function s.synfilter(c)
 	return c:IsFaceup() and c:IsType(TYPE_SYNCHRO) and c:IsLevel(13)
 end
 
--- Activation condition
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetCurrentChain()==0 then return false end
+--Cost
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local b1=Duel.IsExistingMatchingCard(s.xyzfilter,tp,LOCATION_MZONE,0,1,nil,tp)
+	local b2=Duel.IsExistingMatchingCard(s.synfilter,tp,LOCATION_MZONE,0,1,nil)
+	if chk==0 then return b1 or b2 end
+	if not b2 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DEATTACHFROM)
+		local g=Duel.SelectMatchingCard(tp,s.xyzfilter,tp,LOCATION_MZONE,0,1,1,nil,tp)
+		g:GetFirst():RemoveOverlayCard(tp,1,1,REASON_COST)
+	elseif b1 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DEATTACHFROM)
+		local g=Duel.SelectMatchingCard(tp,s.xyzfilter,tp,LOCATION_MZONE,0,1,1,nil,tp)
+		g:GetFirst():RemoveOverlayCard(tp,1,1,REASON_COST)
+	end
+end
+
+--Target
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE_SUMMON,eg,#eg,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,#eg,0,0)
+end
+
+
+--Operation
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.NegateSummon(eg) then
+		Duel.Destroy(eg,REASON_EFFECT)	 
+	end
+end
+function s.condition2(e,tp,eg,ep,ev,re,r,rp)
 	local res=false
 	if re:IsActiveType(TYPE_MONSTER) and re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) then
 		local tg=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS)
@@ -31,33 +76,28 @@ function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	end
 	return (re:IsHasType(EFFECT_TYPE_ACTIVATE) or res) and Duel.IsChainNegatable(ev)
 end
-
--- Xyz material filter
-function s.xyzfilter(c)
-	return c:IsFaceup() and c:IsSetCard(0xf86) and c:IsType(TYPE_XYZ) and c:CheckRemoveOverlayCard(tp,1,REASON_COST)
-end
-
-function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	local b1=Duel.IsExistingMatchingCard(s.xyzfilter,tp,LOCATION_MZONE,0,1,nil)
-	local b2=Duel.IsExistingMatchingCard(s.lv13filter,tp,LOCATION_MZONE,0,1,nil)
+function s.cost2(e,tp,eg,ep,ev,re,r,rp,chk)
+	local b1=Duel.IsExistingMatchingCard(s.xyzfilter,tp,LOCATION_MZONE,0,1,nil,tp)
+	local b2=Duel.IsExistingMatchingCard(s.synfilter,tp,LOCATION_MZONE,0,1,nil)
 	if chk==0 then return b1 or b2 end
-	
-	if b1 and (not b2 or Duel.SelectYesNo(tp,aux.Stringid(id,0))) then
-		local g=Duel.SelectMatchingCard(tp,s.xyzfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	if not b2 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DEATTACHFROM)
+		local g=Duel.SelectMatchingCard(tp,s.xyzfilter,tp,LOCATION_MZONE,0,1,1,nil,tp)
+		g:GetFirst():RemoveOverlayCard(tp,1,1,REASON_COST)
+	elseif b1 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DEATTACHFROM)
+		local g=Duel.SelectMatchingCard(tp,s.xyzfilter,tp,LOCATION_MZONE,0,1,1,nil,tp)
 		g:GetFirst():RemoveOverlayCard(tp,1,1,REASON_COST)
 	end
-	-- If b2 is true and we don't pick b1, we proceed without cost
 end
-
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
-	if re:GetHandler():IsRelateToEffect(re) then
+	if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
 		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
 	end
 end
-
-function s.activate(e,tp,eg,ep,ev,re,r,rp)
+function s.activate2(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) then
 		Duel.Destroy(eg,REASON_EFFECT)
 	end
--- script/c220531.lua
+++ script/c220531.lua
@@ -1,5 +1,66 @@
---Emi - The Shadow Beast
-local s,id,o=GetID()
+local s,id=GetID()
 function s.initial_effect(c)
+	-- 1: Special Summon from hand if you took effect damage
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_DAMAGE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
+	e1:SetCode(EVENT_DAMAGE)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.spcon)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
 	
+	-- 2: On Summon: Search "Shadow Beast" or "Kazari"
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_SUMMON_SUCCESS)
+	e2:SetCountLimit(1,id+100)
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e3)
+end
+
+-- E1: Summon Condition
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return ep==tp and (r&REASON_EFFECT)~=0
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,ev*2)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 then
+		Duel.Damage(1-tp,ev*2,REASON_EFFECT)
+	end
+end
+
+-- E2: Search Logic
+function s.thfilter(c)
+	return (c:IsSetCard(0xd8f) or c:IsCode(220450)) 
+		and c:IsType(TYPE_MONSTER) and not c:IsCode(id) and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
 end
