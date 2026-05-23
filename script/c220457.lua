695fd795e45729b4cdfa09a9eb4a6d888f0c5c92
Nebby <65876365+gmustudio69@users.noreply.github.com>
2026-05-23 10:59:24 +0700

    Fix

--- script/c220457.lua
+++ script/c220457.lua
@@ -1,11 +1,15 @@
 --<Limit Breaker> Shadow Catastrophe
 local s,id=GetID()
 function s.initial_effect(c)
-	-- 1: Battle Protection
+	--Set Limit S/T
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
-	e1:SetValue(1)
+	e1:SetCategory(CATEGORY_LEAVE_GRAVE+CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_PHASE+PHASE_END)
+	e1:SetRange(LOCATION_GRAVE)
+	e1:SetCountLimit(1)
+	e1:SetTarget(s.thtg)
+	e1:SetOperation(s.thop)
 	c:RegisterEffect(e1)
 	-- 2: Place 1 "Limit" Continuous Spell/Trap from Hand or Deck
 	local e2=Effect.CreateEffect(c)
@@ -65,5 +69,25 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc and tc:IsRelateToEffect(e) then
 		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+function s.targetfilter(c)
+	return c:IsDestructable()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.IsExistingMatchingCard(s.targetfilter,tp,LOCATION_ONFIELD,0,1,c,tp)
+		and c:IsAbleToHand() end
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,nil,1,tp,LOCATION_ONFIELD)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,c,1,tp,LOCATION_GRAVE)
+end
+
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local exc=c:IsRelateToEffect(e) and c or nil
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectMatchingCard(tp,s.targetfilter,tp,LOCATION_ONFIELD,0,1,1,exc,tp)
+	if #g>0 and Duel.Destroy(g,REASON_EFFECT)>0 and c:IsRelateToEffect(e) then
+		Duel.SendtoHand(c,REASON_EFFECT)
 	end
 end
