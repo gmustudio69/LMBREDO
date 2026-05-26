a81baa49cb9eaf5ca1c13aaa8e42490b21782000
Nebby <65876365+gmustudio69@users.noreply.github.com>
2026-05-27 04:10:37 +0700

    Update c220410.lua

--- script/c220410.lua
+++ script/c220410.lua
@@ -9,15 +9,15 @@ function s.initial_effect(c)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
 	e1:SetValue(s.splimit)
 	c:RegisterEffect(e1)
-	-- Banishment: Field or Detached to Opponent's GY
+	-- Banisher (Field to GY -> Banish)
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetProperty(EFFECT_FLAG_SET_AVAILABLE+EFFECT_FLAG_IGNORE_RANGE)
+	e2:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
 	e2:SetCode(EFFECT_TO_GRAVE_REDIRECT)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetTargetRange(0,LOCATION_GRAVE)
-	e2:SetValue(LOCATION_REMOVED)
 	e2:SetTarget(s.rmtarget)
+	e2:SetTargetRange(0,LOCATION_ONFIELD)
+	e2:SetValue(LOCATION_REMOVED)
 	c:RegisterEffect(e2)
 
 	-- Attach banished card
@@ -44,13 +44,27 @@ function s.initial_effect(c)
 	e4:SetTarget(s.copytg)
 	e4:SetOperation(s.copyop)
 	c:RegisterEffect(e4)
+	-- Banish instead of GY (Field or Detached)
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_FIELD)
+	e5:SetProperty(EFFECT_FLAG_SET_AVAILABLE+EFFECT_FLAG_IGNORE_RANGE)
+	e5:SetCode(EFFECT_TO_GRAVE_REDIRECT)
+	e5:SetRange(LOCATION_MZONE)
+	e5:SetTargetRange(0,LOCATION_GRAVE)
+	e5:SetValue(LOCATION_REMOVED)
+	e5:SetTarget(s.detachtarget)
+	c:RegisterEffect(e5)
 end
+
 -- E1: Special Summon Limit
 function s.splimit(e,se,sp,st)
 	return se:GetHandler():IsSetCard(0xf86) -- Replace 0xXXXX with your "Limit" SetCode
 end
-function s.rmtarget(e,c,tp,r)
-	return c:GetReason()&(REASON_DESTROY+REASON_DISCARD+REASON_COST+REASON_EFFECT)~=0
+function s.detachtarget(e,c,tp,r)
+	return c:GetOwner()~=e:GetHandlerPlayer() and c:GetReason()&(REASON_COST+REASON_EFFECT)~=0
+end
+function s.rmtarget(e,c)
+	return Duel.IsPlayerCanRemove(e:GetHandlerPlayer(),c)
 end
 function s.filter(c,e)
 	return c:GetOwner()~=e:GetHandlerPlayer() and c:IsLocation(LOCATION_REMOVED) 
