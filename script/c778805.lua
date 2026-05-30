a38dbb81c4cf3eba62bbb424506e557d05c7f6ce
Nebby <65876365+gmustudio69@users.noreply.github.com>
2026-05-30 23:56:56 +0700

    The third

--- script/c778805.lua
+++ script/c778805.lua
@@ -1,10 +1,10 @@
 local s,id=GetID()
 
 function s.initial_effect(c)
-	-- Normal/Special Summon
+	-- Normal Summon
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND+CATEGORY_SPECIAL_SUMMON+CATEGORY_POSITION)
+	e1:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND+CATEGORY_LEAVE_GRAVE+CATEGORY_SPECIAL_SUMMON+CATEGORY_POSITION)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetProperty(EFFECT_FLAG_DELAY)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
@@ -13,6 +13,7 @@ function s.initial_effect(c)
 	e1:SetOperation(s.thop)
 	c:RegisterEffect(e1)
 
+	-- Special Summon
 	local e2=e1:Clone()
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 	c:RegisterEffect(e2)
@@ -24,86 +25,90 @@ function s.initial_effect(c)
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e3:SetProperty(EFFECT_FLAG_DELAY)
 	e3:SetCode(EVENT_REMOVE)
-	e3:SetCountLimit(1,{id,1})
-	e3:SetTarget(s.sptg)
-	e3:SetOperation(s.spop)
+	e3:SetCountLimit(1,{id,2})
+	e3:SetTarget(s.exctg)
+	e3:SetOperation(s.excop)
 	c:RegisterEffect(e3)
 end
--- Mysthich monster
-function s.thfilter(c,e,tp)
-	return c:IsSetCard(0x76b)
-		and c:IsMonster()
-		and (c:IsAbleToHand()
-			or (Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-				and c:IsCanBeSpecialSummoned(e,0,tp,false,false))) and not c:IsCode(id)
+-- Moon Spell/Trap
+function s.moonfilter(c)
+	return c:IsSetCard(0xaaf)
+		and (c:IsSpell() or c:IsTrap())
+		and (c:IsAbleToHand() or c:IsSSetable())
 end
 
--- Moon Gate
-function s.moonfilter(c)
+-- Face-up Moon Gate
+function s.mgfilter(c)
 	return c:IsFaceup() and c:IsCode(778804)
+end
+
+-- Monster in hand
+function s.handspfilter(c,e,tp)
+	return c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEDOWN_DEFENSE)
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
-		return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil,e,tp)
+		return Duel.IsExistingMatchingCard(
+			s.moonfilter,tp,LOCATION_DECK,0,1,nil)
 	end
 end
-
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_OPERATECARD)
-	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,
+		s.moonfilter,tp,LOCATION_DECK,0,1,1,nil)
+
 	local tc=g:GetFirst()
 	if not tc then return end
 
-	local moongate=Duel.IsExistingMatchingCard(
-		s.moonfilter,tp,LOCATION_FZONE,0,1,nil)
+	local setable=tc:IsSSetable()
+	local handable=tc:IsAbleToHand()
 
-	local sp=false
+	if setable and handable then
+		local op=Duel.SelectOption(tp,
+			aux.Stringid(id,2), -- Add to hand
+			aux.Stringid(id,3)) -- Set
 
-	if moongate
-		and tc:IsCanBeSpecialSummoned(e,0,tp,false,false)
-		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.SelectYesNo(tp,
-			aux.Stringid(id,2)) then
-
-		sp=Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)>0
-	end
-
-	if not sp and tc:IsAbleToHand() then
+		if op==0 then
+			Duel.SendtoHand(tc,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,tc)
+		else
+			Duel.SSet(tp,tc)
+		end
+	elseif handable then
 		Duel.SendtoHand(tc,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,tc)
+	elseif setable then
+		Duel.SSet(tp,tc)
 	end
 
+	-- Moon Gate bonus summon
+	if Duel.IsExistingMatchingCard(
+		s.mgfilter,tp,LOCATION_FZONE,0,1,nil)
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(
+			s.handspfilter,tp,LOCATION_HAND,0,1,nil)
+		and Duel.SelectYesNo(tp,aux.Stringid(id,4)) then
+
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+
+		local sg=Duel.SelectMatchingCard(tp,
+			s.handspfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+
+		local sc=sg:GetFirst()
+
+		if sc then
+			Duel.SpecialSummon(
+				sc,0,tp,tp,false,false,
+				POS_FACEDOWN_DEFENSE)
+		end
+	end
+
+	-- Then Set this card
 	if c:IsRelateToEffect(e)
 		and c:IsFaceup()
 		and c:IsCanTurnSet() then
 		Duel.ChangePosition(c,POS_FACEDOWN_DEFENSE)
-	end
-end
-function s.spfilter(c,e,tp)
-	return c:IsCode(id)
-		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-end
-
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then
-		return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-			and Duel.IsExistingMatchingCard(
-				s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp)
-	end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
-end
-
-function s.spop(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,
-		LOCATION_DECK,0,1,1,nil,e,tp)
-
-	local tc=g:GetFirst()
-	if tc then
-		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
