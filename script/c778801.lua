ff6d4c2d77657753e99dc794ec04d7f65d037016
Nebby <65876365+gmustudio69@users.noreply.github.com>
2026-05-30 23:16:10 +0700

    Book of moon

--- script/c778801.lua
+++ script/c778801.lua
@@ -4,7 +4,6 @@ function s.initial_effect(c)
 	-- Link Summon
 	Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsType,TYPE_EFFECT),2)
 	c:EnableReviveLimit()
-
 	-- Special Summon procedure from Extra Deck
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
@@ -14,11 +13,32 @@ function s.initial_effect(c)
 	e1:SetCondition(s.spcon)
 	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
+	-- If this card is Special Summoned
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_DESTROY)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetTarget(s.destg)
+	e2:SetOperation(s.desop)
+	c:RegisterEffect(e2)
+
+	-- If this card is banished
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_LEAVE_GRAVE)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetCode(EVENT_REMOVE)
+	e3:SetTarget(s.settg)
+	e3:SetOperation(s.setop)
+	c:RegisterEffect(e3)
 end
 
 -- "Mysthich" monster
 function s.mysthichfilter(c)
-	return c:IsSetCard(0xb67)
+	return c:IsSetCard(0x76b)
 end
 
 -- Face-down monster
@@ -58,4 +78,75 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
 	Duel.SendtoGrave(sg,REASON_MATERIAL+REASON_LINK)
 
 	c:SetMaterial(sg)
+end
+-- Face-down monster count
+function s.fdcountfilter(c)
+	return c:IsFacedown() and c:IsMonster()
+end
+
+-- Destroy effect
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local ct=Duel.GetMatchingGroupCount(s.fdcountfilter,tp,
+		LOCATION_MZONE,LOCATION_MZONE,nil)+1
+	if chkc then
+		return chkc:IsOnField() and chkc:IsSpellTrap()
+	end
+	if chk==0 then
+		return ct>0 and Duel.IsExistingTarget(Card.IsSpellTrap,tp,
+			LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil)
+	end
+
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,Card.IsSpellTrap,tp,
+		LOCATION_ONFIELD,LOCATION_ONFIELD,1,ct,nil)
+
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
+end
+
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local tg=Duel.GetTargetCards(e)
+	if #tg>0 then
+		Duel.Destroy(tg,REASON_EFFECT)
+	end
+end
+
+-- Moon Spell
+function s.spellfilter(c)
+	return c:IsSetCard(0xaaf)
+		and c:IsSpell()
+		and c:IsSSetable()
+end
+
+-- Moon Trap
+function s.trapfilter(c)
+	return c:IsSetCard(0xaaf)
+		and c:IsTrap()
+		and c:IsSSetable()
+end
+
+function s.settg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		return Duel.GetLocationCount(tp,LOCATION_SZONE)>=2
+			and Duel.IsExistingMatchingCard(s.spellfilter,tp,
+				LOCATION_DECK,0,1,nil)
+			and Duel.IsExistingMatchingCard(s.trapfilter,tp,
+				LOCATION_DECK,0,1,nil)
+	end
+end
+
+function s.setop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_SZONE)<2 then return end
+
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+	local g1=Duel.SelectMatchingCard(tp,s.spellfilter,tp,
+		LOCATION_DECK,0,1,1,nil)
+	if #g1==0 then return end
+
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+	local g2=Duel.SelectMatchingCard(tp,s.trapfilter,tp,
+		LOCATION_DECK,0,1,1,nil)
+	if #g2==0 then return end
+
+	g1:Merge(g2)
+	Duel.SSet(tp,g1)
 end
