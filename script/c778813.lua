b8b47f7b505af15c87b2ba986e2cb7d53a8bd1cf
Nebby <65876365+gmustudio69@users.noreply.github.com>
2026-05-31 23:14:46 +0700

    Update c778813.lua

--- script/c778813.lua
+++ script/c778813.lua
@@ -18,6 +18,16 @@ function s.initial_effect(c)
 	e2:SetCondition(s.lmcon)
 	e2:SetValue(1)
 	c:RegisterEffect(e2)
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetCategory(CATEGORY_DRAW)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e3:SetCountLimit(1,{id,1})
+	e3:SetTarget(s.tgtg)
+	e3:SetOperation(s.tgop)
+	c:RegisterEffect(e3)
 end
 function s.linkmatfilter(c,lc,sumtype,tp)
 	return c:IsSetCard(0x76b) and c:IsLevelAbove(5)
@@ -61,4 +71,150 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
 end
 function s.lmcon(e)
 	return e:GetHandler():GetTurnID()==Duel.GetTurnCount()
+end
+function s.mgfilter(c)
+	return c:IsFaceup()
+		and c:IsCode(778804)
+end
+function s.moonfilter(c)
+	return c:IsCode(778804)
+		and not c:IsForbidden()
+end
+function s.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
+
+	local draw=Duel.IsPlayerCanDraw(tp,1)
+
+	local place=Duel.IsExistingMatchingCard(
+		s.moonfilter,tp,
+		LOCATION_DECK,0,
+		1,nil)
+
+	if chk==0 then
+		return draw or place
+	end
+
+	Duel.SetOperationInfo(
+		0,
+		CATEGORY_DRAW,
+		nil,
+		0,
+		tp,
+		1)
+end
+function s.tgop(e,tp,eg,ep,ev,re,r,rp)
+
+	local mg=Duel.IsExistingMatchingCard(
+		s.mgfilter,tp,
+		LOCATION_FZONE,0,
+		1,nil)
+
+	if not mg then
+
+		local tc=Duel.SelectMatchingCard(
+			tp,
+			s.moonfilter,
+			tp,
+			LOCATION_DECK,
+			0,
+			1,1,nil):GetFirst()
+
+		if tc then
+			Duel.MoveToField(
+				tc,
+				tp,
+				tp,
+				LOCATION_FZONE,
+				POS_FACEUP,
+				true)
+		end
+
+	else
+
+		Duel.Draw(tp,1,REASON_EFFECT)
+	end
+
+	local e1=Effect.CreateEffect(e:GetHandler())
+
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+
+	e1:SetTargetRange(1,0)
+
+	e1:SetTarget(function(e,c)
+		return c:IsLocation(LOCATION_EXTRA)
+			and not c:IsType(TYPE_LINK)
+	end)
+
+	e1:SetReset(RESET_PHASE|PHASE_END)
+
+	Duel.RegisterEffect(e1,tp)
+
+	aux.RegisterClientHint(
+		e:GetHandler(),
+		nil,
+		tp,
+		1,
+		0,
+		"Cannot Special Summon from the Extra Deck except Link Monsters this turn.",
+		nil)
+end
+function s.costfilter(c)
+	return c:IsSetCard(0x76b)
+		and c:IsAbleToRemoveAsCost()
+end
+
+function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+
+	if chk==0 then
+		return Duel.IsExistingMatchingCard(
+			s.costfilter,tp,
+			LOCATION_MZONE,0,
+			1,nil)
+	end
+
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+
+	local g=Duel.SelectMatchingCard(
+		tp,
+		s.costfilter,
+		tp,
+		LOCATION_MZONE,
+		0,
+		1,1,nil)
+
+	Duel.Remove(g,POS_FACEUP,REASON_COST)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+
+	if chk==0 then
+		return e:GetHandler():IsCanBeSpecialSummoned(
+			e,0,tp,false,false)
+	end
+
+	Duel.SetOperationInfo(
+		0,
+		CATEGORY_SPECIAL_SUMMON,
+		e:GetHandler(),
+		1,
+		0,
+		0)
+end
+function s.ssop(e,tp,eg,ep,ev,re,r,rp)
+
+	local c=e:GetHandler()
+
+	if not c:IsRelateToEffect(e) then
+		return
+	end
+
+	Duel.SpecialSummon(
+		c,
+		0,
+		tp,
+		tp,
+		false,
+		false,
+		POS_FACEUP)
 end
