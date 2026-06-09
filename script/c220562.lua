0ef5c2bd61fca404dcb6d7cc7b993c5ce6a6cdb1
Nebby <gmustudio69@gmail.com>
2026-06-09 16:11:54 +0700

    Detonator

---
+++ script/c220562.lua
@@ -0,0 +1,141 @@
+-- Blastcore Detonator
+local s,id=GetID()
+
+function s.initial_effect(c)
+
+	c:EnableReviveLimit()
+
+	---------------------------------------------------
+	-- Replacement: destroyed cards to GY → hand
+	---------------------------------------------------
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_FIELD)
+	e0:SetCode(EFFECT_REPLACE_DESTROY)
+	e0:SetRange(LOCATION_MZONE)
+	e0:SetTarget(s.reptg)
+	e0:SetValue(s.repval)
+	c:RegisterEffect(e0)
+
+	---------------------------------------------------
+	-- Summon effect
+	---------------------------------------------------
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.destg)
+	e1:SetOperation(s.desop)
+	c:RegisterEffect(e1)
+
+	local e2=e1:Clone()
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e2)
+
+	---------------------------------------------------
+	-- Revive + Deck summon on destruction
+	---------------------------------------------------
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_DESTROYED)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetCountLimit(1,{id,1})
+	e3:SetCondition(s.revcon)
+	e3:SetOperation(s.revop)
+	c:RegisterEffect(e3)
+
+end
+
+---------------------------------------------------
+-- REPLACEMENT: destroy → add to hand
+---------------------------------------------------
+function s.repfilter(c)
+	return c:IsReason(REASON_DESTROY)
+		and c:IsLocation(LOCATION_GRAVE)
+		and c:IsAbleToHand()
+end
+
+function s.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		return eg:IsExists(s.repfilter,1,nil)
+	end
+	return true
+end
+
+function s.repval(e,c)
+	return s.repfilter(c)
+end
+
+---------------------------------------------------
+-- DESTROY EFFECT FILTERS
+---------------------------------------------------
+function s.desfilter(c)
+	return (c:IsSetCard(0xc25) and c:IsMonster())
+		or (c:IsAttribute(ATTRIBUTE_FIRE) and c:IsRace(RACE_WARRIOR) and c:IsLevel(7))
+end
+
+---------------------------------------------------
+-- DESTROY TARGET
+---------------------------------------------------
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		return Duel.IsExistingMatchingCard(s.desfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil)
+	end
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,nil,1,tp,LOCATION_HAND+LOCATION_DECK)
+end
+
+---------------------------------------------------
+-- DESTROY OPERATION
+---------------------------------------------------
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+
+	local g=Duel.SelectMatchingCard(tp,s.desfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil)
+	local tc=g:GetFirst()
+
+	if tc then
+		Duel.Destroy(tc,REASON_EFFECT)
+	end
+end
+
+---------------------------------------------------
+-- REVIVE AFTER DESTRUCTION
+---------------------------------------------------
+function s.revcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsReason(REASON_EFFECT)
+end
+
+function s.spfilter(c,e,tp)
+	return c:IsSetCard(0xdet)
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+
+function s.revop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+
+	-- schedule revive at start of next Battle Phase
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_PHASE+PHASE_BATTLE_START)
+	e1:SetCountLimit(1)
+	e1:SetCondition(function() return Duel.GetTurnPlayer()==tp end)
+	e1:SetOperation(function()
+		if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 then
+
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+
+			local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+			if #g>0 then
+				Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+			end
+		end
+	end)
+
+	Duel.RegisterEffect(e1,tp)
+end
