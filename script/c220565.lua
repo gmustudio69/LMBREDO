6b9aa4a832435058196dec266b47c49fd67dfc37
Nebby <gmustudio69@gmail.com>
2026-07-20 17:33:32 +0700

    Update c220565.lua

--- script/c220565.lua
+++ script/c220565.lua
@@ -106,7 +106,7 @@ function s.spcon(e, tp, eg, ep, ev, re, r, rp)
 	return (r & REASON_EFFECT) ~= 0
 end
 function s.spfilter(c, e, tp)
-	return c:IsAttribute(ATTRIBUTE_FIRE) and c:IsLevelByRuleLower(7) and c:IsCanBeSpecialSummoned(e, 0, tp, false, false)
+	return c:IsAttribute(ATTRIBUTE_FIRE) and c:IsLevelBelow(7) and c:IsCanBeSpecialSummoned(e, 0, tp, false, false)
 end
 function s.sptg(e, tp, eg, ep, ev, re, r, rp, chk)
 	if chk == 0 then return Duel.GetLocationCount(tp, LOCATION_MZONE) > 0
