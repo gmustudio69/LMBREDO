b64dd11718ea7015fa561ecbdfe3dc96cc955f87
Nebby <gmustudio69@gmail.com>
2026-07-21 01:05:46 +0700

    l

--- script/c220670.lua
+++ script/c220670.lua
@@ -2,7 +2,7 @@
 local s,id=GetID()
 
 function s.initial_effect(c)
-	-- Activate
+	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
@@ -14,23 +14,33 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 
--- Neither player can activate monster effects in response
+-- Monster effects cannot be chained
 function s.actcon(e,tp,eg,ep,ev,re,r,rp)
-	Duel.SetChainLimitTillChainEnd(s.chainlm)
+	local ge=Effect.GlobalEffect()
+	ge:SetType(EFFECT_TYPE_FIELD)
+	ge:SetCode(EFFECT_CANNOT_INACTIVATE)
+	ge:SetValue(s.effectfilter)
+	ge:SetReset(RESET_CHAIN)
+	Duel.RegisterEffect(ge,tp)
+
+	local ge2=Effect.GlobalEffect()
+	ge2:SetType(EFFECT_TYPE_FIELD)
+	ge2:SetCode(EFFECT_CANNOT_DISEFFECT)
+	ge2:SetValue(s.effectfilter)
+	ge2:SetReset(RESET_CHAIN)
+	Duel.RegisterEffect(ge2,tp)
+
 	return true
 end
 
-function s.chainlm(e,rp,tp)
-	return not re:IsActiveType(TYPE_MONSTER)
+function s.effectfilter(e,ct)
+	local te=Duel.GetChainInfo(ct,CHAININFO_TRIGGERING_EFFECT)
+	return te and te:GetHandler():IsMonster()
 end
 
-----------------------------------------------------------
--- Rank 13 filter
-----------------------------------------------------------
-
-function s.spfilter(c,e,tp)
-	return c:IsType(TYPE_XYZ)
-		and c:IsRank(13)
+function s.xyzfilter(c,e,tp)
+	return c:IsRank(13)
+		and c:IsType(TYPE_XYZ)
 		and c:IsCanBeSpecialSummoned(e,0,tp,true,false)
 end
 
@@ -38,76 +48,61 @@ function s.rescon(sg,e,tp,mg)
 	return sg:GetClassCount(Card.GetCode)==2
 end
 
-----------------------------------------------------------
--- Target
-----------------------------------------------------------
-
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
-		return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-			and Duel.GetLocationCount(1-tp,LOCATION_MZONE)>0
-			and aux.SelectUnselectGroup(
-				Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_EXTRA,0,nil,e,tp),
-				e,tp,2,2,s.rescon,0)
+		return Duel.IsExistingMatchingCard(s.xyzfilter,tp,LOCATION_EXTRA,0,2,nil,e,tp)
 	end
 
-	local g=Duel.GetMatchingGroup(Card.IsMonster,tp,
-		LOCATION_MZONE,LOCATION_MZONE,nil)
-
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,nil,0,PLAYER_ALL,LOCATION_MZONE)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,2,tp,LOCATION_EXTRA)
 end
 
-----------------------------------------------------------
--- Activate
-----------------------------------------------------------
-
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-
-	------------------------------------------------------
-	-- Destroy all monsters
-	------------------------------------------------------
-
-	local dg=Duel.GetMatchingGroup(Card.IsMonster,tp,
-		LOCATION_MZONE,LOCATION_MZONE,nil)
-
-	if #dg>0 then
-		Duel.Destroy(dg,REASON_EFFECT)
-	end
-
-	------------------------------------------------------
-	-- Check zones
-	------------------------------------------------------
+	local dg=Duel.GetMatchingGroup(Card.IsDestructable,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+	Duel.Destroy(dg,REASON_EFFECT)
 
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	if Duel.GetLocationCount(1-tp,LOCATION_MZONE)<=0 then return end
 
-	------------------------------------------------------
-	-- Select 2 Rank 13
-	------------------------------------------------------
-
-	local g=Duel.GetMatchingGroup(s.spfilter,tp,
-		LOCATION_EXTRA,0,nil,e,tp)
+	local g=Duel.GetMatchingGroup(s.xyzfilter,tp,LOCATION_EXTRA,0,nil,e,tp)
 
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 
-	local sg=aux.SelectUnselectGroup(
-		g,e,tp,2,2,s.rescon,1,tp,HINTMSG_SPSUMMON)
-
+	local sg=aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,1,tp,HINTMSG_SPSUMMON)
 	if not sg then return end
 
-	Duel.ConfirmCards(1-tp,sg)
+	local c1,c2=sg:GetFirst(),sg:GetNext()
 
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local you,opp
 
-	local g1=sg:Select(tp,1,1,nil)
-	local tc1=g1:GetFirst()
+	if c1:GetAttack()<=c2:GetAttack() then
+		you=c1
+		opp=c2
+	else
+		you=c2
+		opp=c1
+	end
 
-	sg:RemoveCard(tc1)
-
-	local tc2=sg:GetFirst()
-
-	Duel.SpecialSummonStep(tc1,0,tp,tp,true,false,POS_FACEUP)
-	Duel.SpecialSummonStep(tc2,0,tp,1-tp,true,false,POS_FACEUP)
+	Duel.SpecialSummonStep(you,0,tp,tp,true,false,POS_FACEUP)
+	Duel.SpecialSummonStep(opp,0,tp,1-tp,true,false,POS_FACEUP)
 	Duel.SpecialSummonComplete()
+
+	local rg=Group.FromCards(you,opp)
+
+	for tc in aux.Next(rg) do
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+		e1:SetCode(EVENT_PHASE+PHASE_END)
+		e1:SetCountLimit(1)
+		e1:SetOperation(s.retop)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END)
+		tc:RegisterEffect(e1)
+	end
+end
+
+function s.retop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsLocation(LOCATION_MZONE) then
+		Duel.SendtoDeck(c,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
+	end
 end
