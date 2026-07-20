0bb44fede63cc64a88210402877ab3e4a7f3b484
Nebby <gmustudio69@gmail.com>
2026-07-20 08:26:42 +0700

    Update c220567.lua

--- script/c220567.lua
+++ script/c220567.lua
@@ -1,6 +1,11 @@
 --Pyre - Limit Break!!!
 local s,id,o=GetID()
 function s.initial_effect(c)
+	-- Activate: Continuous Spell
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_ACTIVATE)
+	e0:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e0)
 	-- Destruction protection
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
