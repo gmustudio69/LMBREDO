f76dc903f2b00d7119bfadfc7ba24aea3ef9dab7
Nebby <gmustudio69@gmail.com>
2026-07-20 20:07:21 +0700

    Flamewings

---
+++ script/c220441.lua
@@ -0,0 +1,88 @@
+local s,id=GetID()
+function s.initial_effect(c)
+	--Xyz Summon
+	Xyz.AddProcedure(c,nil,10,2,s.xyzfilter,aux.Stringid(id,0),2,s.xyzop)
+	c:EnableReviveLimit()
+	--Detach 2: Destroy all other monsters except Ellie
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,1))
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1,id)
+	e1:SetCost(Cost.DetachFromSelf(2)) -- Detach 2 materials
+	e1:SetTarget(s.destg)
+	e1:SetOperation(s.desop)
+	c:RegisterEffect(e1)
+
+	--If sent to GY: Shuffle up to 3 FIRE monsters from GY or Banishment into Deck
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,2))
+	e2:SetCategory(CATEGORY_TODECK)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+	e2:SetCode(EVENT_TO_GY)
+	e2:SetCountLimit(1,id+100)
+	e2:SetTarget(s.tdtg)
+	e2:SetOperation(s.tdop)
+	c:RegisterEffect(e2)
+end
+function s.checkop(e,tp,eg,ep,ev,re,r,rp)
+	local a=Duel.GetAttacker()
+	local d=Duel.GetAttackTarget()
+	if (a and a:IsRace(RACE_WARRIOR) and a:IsAttribute(ATTRIBUTE_FIRE)) or (d and d:IsRace(RACE_WARRIOR) and d:IsAttribute(ATTRIBUTE_FIRE)) then
+		Duel.RegisterFlagEffect(0,id+1,RESET_PHASE|PHASE_END,0,1)
+	end
+end
+function s.xyzfilter(c,tp,xyzc)
+	return c:IsFaceup() and c:IsRace(RACE_WARRIOR) and c:IsAttribute(ATTRIBUTE_FIRE)
+end
+function s.xyzop(e,tp,chk)
+	if chk==0 then return Duel.GetFlagEffect(tp,id)==0 and Duel.GetFlagEffect(0,id+1)>0 end
+	Duel.RegisterFlagEffect(tp,id,RESET_PHASE|PHASE_END,0,1)
+	return true
+end
+-- 1. Mass Destruction Logic (Except Ellie)
+function s.elliefilter(c)
+	return c:IsFaceup() and c:IsCode(220405) -- Replace 98765432 with the exact card ID of "<World Decoder> Ellie"
+end
+
+function s.desfilter(c)
+	return c:IsType(TYPE_MONSTER) and not s.elliefilter(c)
+end
+
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.IsExistingMatchingCard(s.desfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,c) end
+	local g=Duel.GetMatchingGroup(s.desfilter,tp,LOCATION_MZONE,LOCATION_MZONE,c)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
+end
+
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local g=Duel.GetMatchingGroup(s.desfilter,tp,LOCATION_MZONE,LOCATION_MZONE,c)
+	if #g>0 then
+		Duel.Destroy(g,REASON_EFFECT)
+	end
+end
+
+-- 2. GY / Banishment Recycling Logic
+function s.tdfilter(c)
+	return c:IsAttribute(ATTRIBUTE_FIRE) and c:IsType(TYPE_MONSTER) and c:IsAbleToDeck()
+end
+
+function s.tdtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GY|LOCATION_REMOVED) and chkc:IsControler(tp) and s.tdfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.tdfilter,tp,LOCATION_GY|LOCATION_REMOVED,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g=Duel.SelectTarget(tp,s.tdfilter,tp,LOCATION_GY|LOCATION_REMOVED,0,1,3,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,#g,0,0)
+end
+
+function s.tdop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	local tg=g:Filter(Card.IsRelateToEffect,nil,e)
+	if #tg>0 then
+		Duel.SendtoDeck(tg,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
+	end
+end
