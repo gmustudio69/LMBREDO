b9e24930a0e85e4704a092f54d068dfae94eb712
Nebby <gmustudio69@gmail.com>
2026-07-23 17:10:11 +0700

    Nocturne Gear trap

--- script/c220516.lua
+++ script/c220516.lua
@@ -1,106 +1,92 @@
--- Axiarch Ignition - Edicts
-local s, id = GetID()
-local SET_AXIARCH = 0xe1f -- REPLACE THIS with the actual Setcode you assigned to "Axiarch" in your database!
+--Nocturne Gear - Assemble!!!
+local s,id=GetID()
 function s.initial_effect(c)
-	-- Effect 1: Special Summon from Hand
-	local e1 = Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id, 0))
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
-	e1:SetCode(EFFECT_SPSUMMON_PROC)
-	e1:SetRange(LOCATION_HAND)
-	-- Hard Once Per Turn for this inherent summon method
+	--Effect 1: Special Summon 1 "Nocturne Gear" from GY/Banishment, then optional banish 1 card on field
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_REMOVE)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER_E)
 	e1:SetCountLimit(1,id)
-	e1:SetCondition(s.spcon)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
 
-	-- Effect 2: Search/Salvage 1 "Axiarch" Spell/Trap
-	local e2 = Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id, 1))
-	e2:SetCategory(CATEGORY_TOHAND + CATEGORY_SEARCH)
-	e2:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
-	e2:SetProperty(EFFECT_FLAG_DELAY)
-	e2:SetCode(EVENT_SUMMON_SUCCESS)
-	e2:SetCountLimit(1,{id,1}) -- First effect HOPT
+	--Effect 2: Banish from GY to Add 1 "Nocturne Gear" monster from Deck/Banishment to hand
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1,id)
+	e2:SetCost(aux.bfgcost) -- Banishes this card from GY as cost
 	e2:SetTarget(s.thtg)
 	e2:SetOperation(s.thop)
 	c:RegisterEffect(e2)
-	local e3 = e2:Clone()
-	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
-	c:RegisterEffect(e3)
-
-	-- Effect 3: Destroy & Burn when sent to GY for an Xyz monster's effect
-	local e4 = Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(id, 2))
-	e4:SetCategory(CATEGORY_DESTROY + CATEGORY_DAMAGE)
-	e4:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
-	e4:SetProperty(EFFECT_FLAG_DELAY + EFFECT_FLAG_CARD_TARGET)
-	e4:SetCode(EVENT_TO_GRAVE)
-	e4:SetCountLimit(1,{id,2}) -- Second effect HOPT
-	e4:SetCondition(s.descon)
-	e4:SetTarget(s.destg)
-	e4:SetOperation(s.desop)
-	c:RegisterEffect(e4)
 end
 
--- =========================================================
--- Effect 1: Special Summon Condition
--- =========================================================
-function s.spfilter(c)
-	return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_EARTH)
+-- Filter for "Nocturne Gear" monster in GY or Banishment
+function s.spfilter(c,e,tp)
+	return c:IsSetCard(0xd8f) -- Replace 0xd8f with your official Nocturne Gear archetype code
+		and c:IsType(TYPE_MONSTER)
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+		and (not c:IsLocation(LOCATION_REMOVED) or c:IsFaceup())
 end
-function s.spcon(e, c)
-	if c == nil then return true end
-	local tp = c:GetControler()
-	return Duel.GetLocationCount(tp, LOCATION_MZONE) > 0
-		and Duel.IsExistingMatchingCard(s.spfilter, tp, LOCATION_MZONE, 0, 1, nil)
+
+-- Filter for Psychic Synchro monster control check
+function s.psychicfilter(c)
+	return c:IsFaceup() and c:IsRace(RACE_PSYCHIC) and c:IsType(TYPE_SYNCHRO)
 end
-function s.thfilter(c)
-	return c:IsSetCard(SET_AXIARCH) and c:IsType(TYPE_SPELL + TYPE_TRAP) and c:IsAbleToHand()
+
+-- 1. Special Summon & Conditional Banish Logic
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE|LOCATION_REMOVED,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE|LOCATION_REMOVED)
 end
-function s.thtg(e, tp, eg, ep, ev, re, r, rp, chk)
-	if chk == 0 then return Duel.IsExistingMatchingCard(s.thfilter, tp, LOCATION_DECK + LOCATION_GRAVE, 0, 1, nil) end
-	Duel.SetOperationInfo(0, CATEGORY_TOHAND, nil, 1, tp, LOCATION_DECK + LOCATION_GRAVE)
-end
-function s.thop(e, tp, eg, ep, ev, re, r, rp)
-	Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_ATOHAND)
-	-- aux.NecroValleyFilter ensures it doesn't bypass Necrovalley if pulling from GY
-	local g = Duel.SelectMatchingCard(tp, aux.NecroValleyFilter(s.thfilter), tp, LOCATION_DECK + LOCATION_GRAVE, 0, 1, 1, nil)
-	if #g > 0 then
-		Duel.SendtoHand(g, nil, REASON_EFFECT)
-		Duel.ConfirmCards(1 - tp, g)
+
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE|LOCATION_REMOVED,0,1,1,nil,e,tp)
+	
+	if #g>0 and Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)>0 then
+		-- Optional Banish Check: Requires a face-up Psychic Synchro on your field
+		local rm_g=Duel.GetMatchingGroup(Card.IsAbleToRemove,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
+		if Duel.IsExistingMatchingCard(s.psychicfilter,tp,LOCATION_MZONE,0,1,nil)
+			and #rm_g>0
+			and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+			
+			Duel.BreakEffect()
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+			local sg=rm_g:Select(tp,1,1,nil)
+			Duel.HintSelection(sg)
+			Duel.Remove(sg,POS_FACEUP,REASON_EFFECT)
+		end
 	end
 end
 
--- =========================================================
--- Effect 3: Destroy & Burn
--- =========================================================
-function s.descon(e, tp, eg, ep, ev, re, r, rp)
-	return re and re:GetHandler():IsType(TYPE_XYZ) and (r & REASON_COST) ~= 0
+-- Filter for adding "Nocturne Gear" monster from Deck/Banishment to Hand
+function s.thfilter(c)
+	return c:IsSetCard(0xd8f) -- Replace 0xd8f with your Nocturne Gear archetype code
+		and c:IsType(TYPE_MONSTER)
+		and c:IsAbleToHand()
+		and (not c:IsLocation(LOCATION_REMOVED) or c:IsFaceup())
 end
-function s.destg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1 - tp) end
-	-- "Target 1 monster your opponent controls" - Nil filter allows both Face-Up and Face-Down targets
-	if chk == 0 then return Duel.IsExistingTarget(nil, tp, 0, LOCATION_MZONE, 1, nil) end
-	Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_DESTROY)
-	local g = Duel.SelectTarget(tp, nil, tp, 0, LOCATION_MZONE, 1, 1, nil)
-	Duel.SetOperationInfo(0, CATEGORY_DESTROY, g, 1, 0, 0)
-	-- Pre-calculate damage for the preview window
-	local tc = g:GetFirst()
-	local atk = 0
-	if tc:IsFaceup() then atk = tc:GetBaseAttack() end
-	if atk < 0 then atk = 0 end
-	Duel.SetOperationInfo(0, CATEGORY_DAMAGE, nil, 0, 1 - tp, math.floor(atk / 2))
+
+-- 2. Search Logic
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK|LOCATION_REMOVED,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK|LOCATION_REMOVED)
 end
-function s.desop(e, tp, eg, ep, ev, re, r, rp)
-	local tc = Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) then
-		-- Gets Original ATK
-		local atk = tc:GetBaseAttack() 
-		if atk < 0 then atk = 0 end
-		-- Destroy it, and if you do, inflict damage
-		if Duel.Destroy(tc, REASON_EFFECT) > 0 then
-			Duel.Damage(1 - tp, math.floor(atk / 2), REASON_EFFECT)
-		end
+
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK|LOCATION_REMOVED,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
 	end
 end
--- script/c220567.lua
+++ script/c220567.lua
@@ -130,5 +130,5 @@ end
 
 -- Filter: Allows only Mandatory and Optional Trigger Effects
 function s.actlimit(e,re,tp)
-	return true
+	return not (re:IsHasType(EFFECT_TYPE_TRIGGER_O) or re:IsHasType(EFFECT_TYPE_TRIGGER_F))
 end
