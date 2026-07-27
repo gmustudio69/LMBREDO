ada09a24e0e560ff638b9b124c7162e515f582cc
Nebby <gmustudio69@gmail.com>
2026-07-28 00:07:08 +0700

    Update c220514.lua

--- script/c220514.lua
+++ script/c220514.lua
@@ -1,68 +1,73 @@
--- Limit Overdrive - Resonance
-local s, id = GetID()
+--Limit Overdrive - Resonance
+local s,id=GetID()
+
 function s.initial_effect(c)
-	-- Activate
-	local e1 = Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_DISABLE)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_NEGATE)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetHintTiming(0, TIMINGS_CHECK_MONSTER + TIMING_END_PHASE)
-	e1:SetCountLimit(1, id, EFFECT_COUNT_CODE_OATH)
-	e1:SetCost(s.cost)
+	e1:SetCode(EVENT_CHAINING)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetCondition(s.condition)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
+s.listed_names={220450,id}
+--Can activate when a chain can be responded to
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsChainNegatable(ev)
+end
 
--- Filter for valid tributes: "<Limit Breaker> Kazari" on the field (Monster Zone or face-up Monster treated as Spell)
-function s.tribute_filter(c, tp)
+--"Limit Breaker Kazari"
+function s.cfilter(c)
 	return c:IsFaceup() and c:IsCode(220450)
-		and (c:IsLocation(LOCATION_MZONE) or (c:IsLocation(LOCATION_SZONE) and c:IsType(TYPE_MONSTER)))
+		and (c:IsLocation(LOCATION_MZONE) or c:IsLocation(LOCATION_SZONE))
 		and c:IsReleasable()
 end
 
-function s.cost(e, tp, eg, ep, ev, re, r, rp, chk)
-	if chk == 0 then return Duel.CheckReleaseGroup(tp, s.tribute_filter, 1, nil, tp) end
-	
-	-- Let the player choose any number of Kazari cards to tribute
-	local rg = Duel.SelectReleaseGroup(tp, s.tribute_filter, 1, 99, nil, tp)
-	local ct = #rg
-	
-	-- Perform the tribute cost
-	Duel.Release(rg, REASON_COST)
-	
-	-- Store the tribute count using SetLabel so target/activation know how many cards to select
-	e:SetLabel(ct)
-end
-s.listed_names={220450,id}
-function s.target(e, tp, eg, ep, ev, re, r, rp, chk)
-	local ct = e:GetLabel()
-	if chk == 0 then return ct > 0 and Duel.IsExistingTarget(Card.IsNegatable, tp, LOCATION_ONBOARD, LOCATION_ONBOARD, ct, nil) end
-	
-	-- Target cards on the field equal to the number tributed
-	Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_DISABLE)
-	local g = Duel.SelectTarget(tp, Card.IsNegatable, tp, LOCATION_ONBOARD, LOCATION_ONBOARD, ct, ct, nil)
-	Duel.SetOperationInfo(0, CATEGORY_DISABLE, g, ct, 0, 0)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local g=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_MZONE|LOCATION_SZONE,0,nil)
+	if chkc then
+		return chkc:IsOnField()
+	end
+	if chk==0 then
+		return #g>0
+	end
+
+	local max=math.min(#g,Duel.GetFieldGroupCount(tp,LOCATION_ONFIELD,LOCATION_ONFIELD))
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
+	local rg=g:Select(tp,1,max,nil)
+	e:SetLabel(#rg)
+	Duel.Release(rg,REASON_COST)
+
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local tg=Duel.SelectTarget(tp,Card.IsOnField,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,#rg,#rg,nil)
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,tg,#tg,0,0)
 end
 
-function s.activate(e, tp, eg, ep, ev, re, r, rp)
-	local g = Duel.GetChainInfo(0, CHAININFO_TARGET_CARDS)
-	if not g then return end
-	
-	for tc in aux.Next(g) do
-		if tc:IsRelateToEffect(e) and tc:IsFaceup() and not tc:IsDisabled() then
-			-- Negate effects (permanent or standard depending on design choice; keeping standard persistent negation)
-			Duel.NegateRelatedChain(tc, RESET_TURN_SET)
-			local e1 = Effect.CreateEffect(e:GetHandler())
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local tg=Duel.GetTargetCards(e)
+	for tc in aux.Next(tg) do
+		if tc:IsRelateToEffect(e) then
+			local e1=Effect.CreateEffect(e:GetHandler())
 			e1:SetType(EFFECT_TYPE_SINGLE)
 			e1:SetCode(EFFECT_DISABLE)
-			e1:SetReset(RESET_EVENT + RESETS_STANDARD)
+			e1:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END)
 			tc:RegisterEffect(e1)
-			local e2 = Effect.CreateEffect(e:GetHandler())
+
+			local e2=Effect.CreateEffect(e:GetHandler())
 			e2:SetType(EFFECT_TYPE_SINGLE)
 			e2:SetCode(EFFECT_DISABLE_EFFECT)
-			e2:SetReset(RESET_EVENT + RESETS_STANDARD)
+			e2:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END)
 			tc:RegisterEffect(e2)
+
+			if tc:IsType(TYPE_TRAPMONSTER) then
+				local e3=e2:Clone()
+				e3:SetCode(EFFECT_DISABLE_TRAPMONSTER)
+				tc:RegisterEffect(e3)
+			end
 		end
 	end
 end
