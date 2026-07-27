6449e38524e4eeb3f391cd205b30e37910fe61bb
Nebby <gmustudio69@gmail.com>
2026-07-27 16:53:28 +0700

    Resonant search

---
+++ script/c220542.lua
@@ -0,0 +1,36 @@
+--Resonant <S> - Initialization
+local s,id=GetID()
+function s.initial_effect(c)
+	-- Activate: Add 1 "Resonant" card from Deck to hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+
+-- Archetype definition (Replace 0x99a with your actual "Resonant" setcode)
+local SET_RESONANT = 726
+
+-- Filter for "Resonant" cards in Deck
+function s.thfilter(c)
+	return c:IsSetCard(SET_RESONANT) and c:IsAbleToHand()
+end
+
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
