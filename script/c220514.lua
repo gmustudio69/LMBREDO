9b17e8671a4093d8f9f7871da98eb9ac3b8122ff
Nebby <gmustudio69@gmail.com>
2026-07-28 00:12:00 +0700

    kazari

--- script/c220514.lua
+++ script/c220514.lua
@@ -1,73 +1,72 @@
 --Limit Overdrive - Resonance
 local s,id=GetID()
 
+local KAZARI_ID=220450 -- Change to your Kazari card ID
+
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_NEGATE)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_CHAINING)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
-	e1:SetCondition(s.condition)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id)
+	e1:SetCost(s.cost)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-s.listed_names={220450,id}
---Can activate when a chain can be responded to
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.IsChainNegatable(ev)
-end
 
---"Limit Breaker Kazari"
+--Kazari filter
 function s.cfilter(c)
-	return c:IsFaceup() and c:IsCode(220450)
-		and (c:IsLocation(LOCATION_MZONE) or c:IsLocation(LOCATION_SZONE))
+	return c:IsCode(KAZARI_ID)
+		and (c:IsFaceup() or c:IsLocation(LOCATION_MZONE))
 		and c:IsReleasable()
 end
 
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	local g=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_MZONE|LOCATION_SZONE,0,nil)
-	if chkc then
-		return chkc:IsOnField()
-	end
+--Cost
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
-		return #g>0
+		return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE|LOCATION_SZONE,0,1,nil)
 	end
-
-	local max=math.min(#g,Duel.GetFieldGroupCount(tp,LOCATION_ONFIELD,LOCATION_ONFIELD))
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
-	local rg=g:Select(tp,1,max,nil)
-	e:SetLabel(#rg)
-	Duel.Release(rg,REASON_COST)
-
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	local tg=Duel.SelectTarget(tp,Card.IsOnField,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,#rg,#rg,nil)
-	Duel.SetOperationInfo(0,CATEGORY_NEGATE,tg,#tg,0,0)
+	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_MZONE|LOCATION_SZONE,0,1,99,nil)
+	e:SetLabel(#g)
+	Duel.Release(g,REASON_COST)
 end
 
+--Target
+function s.tgfilter(c)
+	return c:IsFaceup() and c:IsNegatable()
+end
+
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local ct=e:GetLabel()
+	if chkc then
+		return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_ONFIELD) and s.tgfilter(chkc)
+	end
+	if chk==0 then
+		return ct>0
+			and Duel.IsExistingTarget(s.tgfilter,tp,0,LOCATION_ONFIELD,ct,nil)
+	end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_NEGATE)
+	local g=Duel.SelectTarget(tp,s.tgfilter,tp,0,LOCATION_ONFIELD,ct,ct,nil)
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,g,#g,0,0)
+end
+
+--Operation
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local tg=Duel.GetTargetCards(e)
 	for tc in aux.Next(tg) do
-		if tc:IsRelateToEffect(e) then
+		if tc:IsFaceup() and tc:IsRelateToEffect(e) then
+			--Negate its effects while it remains face-up
 			local e1=Effect.CreateEffect(e:GetHandler())
 			e1:SetType(EFFECT_TYPE_SINGLE)
 			e1:SetCode(EFFECT_DISABLE)
-			e1:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END)
+			e1:SetReset(RESET_EVENT|RESETS_STANDARD)
 			tc:RegisterEffect(e1)
 
-			local e2=Effect.CreateEffect(e:GetHandler())
-			e2:SetType(EFFECT_TYPE_SINGLE)
+			local e2=e1:Clone()
 			e2:SetCode(EFFECT_DISABLE_EFFECT)
-			e2:SetReset(RESET_EVENT|RESETS_STANDARD|RESET_PHASE|PHASE_END)
 			tc:RegisterEffect(e2)
-
-			if tc:IsType(TYPE_TRAPMONSTER) then
-				local e3=e2:Clone()
-				e3:SetCode(EFFECT_DISABLE_TRAPMONSTER)
-				tc:RegisterEffect(e3)
-			end
 		end
 	end
 end
