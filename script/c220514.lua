6751d232bad86dbc51d5c9e2bb3f0ca845c8d130
Nebby <gmustudio69@gmail.com>
2026-07-27 23:54:32 +0700

    Update c220514.lua

--- script/c220514.lua
+++ script/c220514.lua
@@ -1,76 +1,74 @@
 -- Limit Overdrive - Resonance
 local s, id = GetID()
-function s.initial_effect(c)
-	-- Activate
-	local e1=Effect.CreateEffect(c)
+function s.initial_effect(e0)
+	-- Activate: Tribute any number of "<Limit Breaker> Kazari" (Monster or S&T Zone), then negate target cards equal to that number
+	local e1 = Effect.CreateEffect(e0:GetHandler())
 	e1:SetCategory(CATEGORY_DISABLE)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetHintTiming(0, TIMINGS_CHECK_MONSTER + TIMING_END_PHASE)
 	e1:SetCountLimit(1, id, EFFECT_COUNT_CODE_OATH)
+	e1:SetCost(s.cost)
 	e1:SetTarget(s.target)
-	e1:SetOperation(s.activate)
-	c:RegisterEffect(e1)
+	e1:SetOperation(s.operation)
+	e0:GetHandler():RegisterEffect(e1)
 end
 
--- Filter for valid tributes: "<Limit Breaker> Kazari" on the field (Monster Zone or Face-up Spell & Trap Zone)
-function s.tribute_filter(c, tp)
-	return c:IsFaceup() and (c:IsCode(220450)) -- Replace 100000000 with "<Limit Breaker> Kazari"'s actual passcode
-		and (c:IsControler(tp) and (c:IsLocation(LOCATION_MZONE) or (c:IsLocation(LOCATION_SZONE) and c:IsType(TYPE_MONSTER))))
-		and Duel.IsExistingMatchingCard(s.negate_filter, tp, 0, LOCATION_ONBOARD, 1, nil) -- at least 1 valid target exists
+-- Filter for valid tribute targets: "<Limit Breaker> Kazari" on the field (Monster or S&T Zone)
+function s.costfilter(c, tp)
+	return c:IsFaceup() and c:IsCode(220450) -- Replace 100000000 with "<Limit Breaker> Kazari"'s actual passcode
+		and (c:IsLocation(LOCATION_MZONE) or c:IsLocation(LOCATION_SZONE))
+		and c:IsReleasable()
+		and Duel.IsExistingTarget(s.negfilter, tp, LOCATION_ONBOARD, LOCATION_ONBOARD, 1, c)
 end
 
-function s.negate_filter(c)
+function s.negfilter(c)
 	return c:IsFaceup() and not c:IsDisabled()
 end
 
-function s.target(e, tp, eg, ep, ev, re, r, rp, chk)
-	local rg = Duel.GetReleaseGroup(tp):Filter(function(c)
-		return c:IsFaceup() and c:IsCode(220450) -- Replace 100000000 with Kazari's passcode
-			and (c:IsLocation(LOCATION_MZONE) or (c:IsLocation(LOCATION_SZONE) and c:IsType(TYPE_MONSTER)))
-	end, nil)
-
-	if chk == 0 then
-		return #rg > 0 and Duel.IsExistingMatchingCard(Card.IsNegatable, tp, LOCATION_ONBOARD, LOCATION_ONBOARD, 1, nil)
-	end
-
-	-- Let the player choose any number of Kazari cards to tribute
-	Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_RELEASE)
-	local tg = rg:Select(tp, 1, #rg, nil)
-	local ct = #tg
-	
-	-- Save the tribute count into the effect structure to use in the operation
-	e:SetLabel(ct)
-	
-	-- Perform the tribute cost immediately as part of target/cost evaluation or setup
-	Duel.Release(tg, REASON_COST)
-
-	-- Target cards on the field equal to the number tributed
-	Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_DISABLE)
-	local g = Duel.SelectTarget(tp, Card.IsNegatable, tp, LOCATION_ONBOARD, LOCATION_ONBOARD, ct, ct, nil)
-	Duel.SetOperationInfo(0, CATEGORY_DISABLE, g, ct, 0, 0)
+-- Cost function: Tribute any number of them and store the count to dynamically target cards
+function s.cost(e, tp, eg, ep, ev, re, r, rp, chk)
+	e:SetLabel(100)
+	if chk == 0 then return true end
 end
 
-function s.activate(e, tp, eg, ep, ev, re, r, rp)
-	local g = Duel.GetChainInfo(0, CHAININFO_TARGET_CARDS)
-	if not g then return end
+function s.target(e, tp, eg, ep, ev, re, r, rp, chk)
+	if chk == 0 then
+		if e:GetLabel() ~= 100 then return false end
+		e:SetLabel(0)
+		return Duel.CheckReleaseGroupCost(tp, s.costfilter, 1, false, nil, nil, tp)
+	end
+	e:SetLabel(0)
 	
-	local tc = g:GetFirst()
-	while tc do
-		if tc:IsRelateToEffect(e) and tc:IsFaceup() and not tc:IsDisabled() then
+	-- Prompt player to select how many "<Limit Breaker> Kazari" to tribute
+	local rg = Duel.SelectReleaseGroupCost(tp, s.costfilter, 1, 99, false, nil, nil, tp)
+	local ct = #rg
+	Duel.Release(rg,REASON_COST)
+	
+	-- Save the number tributed so we can target exactly that many cards
+	e:SetLabel(ct)
+	
+	Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_NEGATE)
+	local tg = Duel.SelectTarget(tp, s.negfilter, tp, LOCATION_ONBOARD, LOCATION_ONBOARD, ct, ct, nil)
+	Duel.SetOperationInfo(0, CATEGORY_DISABLE, tg, #tg, 0, 0)
+end
+
+function s.operation(e, tp, eg, ep, ev, re, r, rp)
+	local g = Duel.GetChainInfo(0, CHAININFO_TARGET_CARDS)
+	local tg = g:Filter(Card.IsRelateToEffect, nil)
+	for tc in aux.Next(tg) do
+		if tc:IsFaceup() and not tc:IsDisabled() then
 			-- Negate effects
-			Duel.NegateRelatedChain(tc, RESET_TURN_SET)
 			local e1 = Effect.CreateEffect(e:GetHandler())
 			e1:SetType(EFFECT_TYPE_SINGLE)
 			e1:SetCode(EFFECT_DISABLE)
-			e1:SetReset(RESET_EVENT + RESETS_STANDARD + RESET_PHASE + PHASE_END)
+			e1:SetReset(RESET_EVENT + RESETS_STANDARD)
 			tc:RegisterEffect(e1)
 			local e2 = Effect.CreateEffect(e:GetHandler())
 			e2:SetType(EFFECT_TYPE_SINGLE)
 			e2:SetCode(EFFECT_DISABLE_EFFECT)
-			e2:SetReset(RESET_EVENT + RESETS_STANDARD + RESET_PHASE + PHASE_END)
+			e2:SetReset(RESET_EVENT + RESETS_STANDARD)
 			tc:RegisterEffect(e2)
 		end
-		tc = g:GetNext()
 	end
 end
