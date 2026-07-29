0b97b3065d89649b2bd621dcb3814314085beff6
Nebby <gmustudio69@gmail.com>
2026-07-29 10:06:54 +0700

    Eve

--- script/c220421.lua
+++ script/c220421.lua
@@ -1,115 +1,119 @@
---Eve Ashcroft
+--Eve Ashcroft, herald of the Endless
 local s,id=GetID()
 function s.initial_effect(c)
-
-	--Tribute from hand/field to set Diagram System
+	-- Treat as a Tuner if used as Synchro Material for a DARK Synchro Monster
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_IGNITION)
-	e1:SetCategory(CATEGORY_SET)
-	e1:SetRange(LOCATION_HAND+LOCATION_MZONE)
-	e1:SetCountLimit(1,id)
-	e1:SetCost(s.setcost)
-	e1:SetTarget(s.settg)
-	e1:SetOperation(s.setop)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetCode(EFFECT_NONTUNER_IS_TUNER)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetValue(s.tuner val)
 	c:RegisterEffect(e1)
 
-	--GY revive when counter trap sent
+	-- Quick Effect: Tribute from hand/field to search "Diagram System" or "Endless" monster
 	local e2=Effect.CreateEffect(c)
-	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_DESTROY)
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e2:SetCode(EVENT_TO_GRAVE)
-	e2:SetRange(LOCATION_GRAVE)
-	e2:SetProperty(EFFECT_FLAG_DELAY)
-	e2:SetCountLimit(1,id+1)
-	e2:SetCondition(s.spcon)
-	e2:SetTarget(s.sptg)
-	e2:SetOperation(s.spop)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_HAND+LOCATION_MZONE)
+	e2:SetHintTiming(0,TIMING_MAIN_END)
+	e2:SetCountLimit(1,id)
+	e2:SetCondition(s.thcon)
+	e2:SetCost(s.thcost)
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
 	c:RegisterEffect(e2)
 
+	-- GY Trigger: If a Counter Trap is sent to GY, Special Summon this card, then optionally destroy 1 card
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_DESTROY)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetCode(EVENT_TO_GRAVE)
+	e3:SetRange(LOCATION_GRAVE)
+	e3:SetCountLimit(1,id+100)
+	e3:SetCondition(s.spcon)
+	e3:SetTarget(s.sptg)
+	e3:SetOperation(s.spop)
+	c:RegisterEffect(e3)
 end
 
---Tribute cost
-function s.setcost(e,tp,eg,ep,ev,re,r,rp,chk)
-local c=e:GetHandler()
-	if chk==0 then
-		if c:IsLocation(LOCATION_HAND) then
-			return true
-		else
-			return c:IsReleasable()
-		end
-	end
-	if c:IsLocation(LOCATION_HAND) then
-		Duel.SendtoGrave(c,REASON_COST)
-	else
-		Duel.Release(c,REASON_COST)
-	end
+-- ==========================================
+-- Definitions & Setcodes
+-- ==========================================
+local DIAGRAM_SYSTEM = 220402 -- Replace with your actual "Diagram System" setcode
+local SET_ENDLESS		= 0xa58 -- Replace with your actual "Endless" setcode
+
+-- ==========================================
+-- E1: DARK Synchro Tuner Treatment
+-- ==========================================
+function s.tunerval(e,sc)
+	return sc and sc:IsAttribute(ATTRIBUTE_DARK) and sc:IsType(TYPE_SYNCHRO)
 end
 
-function s.setfilter(c)
-return c:IsCode(220402) and c:IsSSetable()
+-- ==========================================
+-- E2: Search Logic
+-- ==========================================
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsMainPhase()
 end
 
-function s.settg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then
-		return Duel.IsExistingMatchingCard(s.setfilter,tp,LOCATION_DECK,0,1,nil)
-	end
-end
-
-function s.setop(e,tp,eg,ep,ev,re,r,rp)
+function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
-	local g=Duel.SelectMatchingCard(tp,s.setfilter,tp,LOCATION_DECK,0,1,1,nil)
-	local tc=g:GetFirst()
+	if chk==0 then return c:IsReleasable() and (c:IsLocation(LOCATION_HAND) or c:IsFaceup()) end
+	Duel.Release(c,REASON_COST)
+end
 
-	if tc then
-		Duel.SSet(tp,tc)
+function s.thfilter(c)
+	return c:IsCode(DIAGRAM_SYSTEM) or ( c:IsType(TYPE_MONSTER) and c:IsSetCard(SET_ENDLESS)) and c:IsAbleToHand()
+end
+
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
 	end
-
-	--Special Summon restriction
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
-	e1:SetDescription(aux.Stringid(id,1))
-	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
-	e1:SetTargetRange(1,0)
-	e1:SetTarget(s.splimit)
-	e1:SetReset(RESET_PHASE+PHASE_END)
-	Duel.RegisterEffect(e1,tp)
 end
 
-function s.splimit(e,c,sump,sumtype,sumpos,targetp,se)
-	return c:IsLocation(LOCATION_DECK|LOCATION_EXTRA) and not c:IsAttribute(ATTRIBUTE_DARK)
-end
-
---COUNTER TRAP TO GY CONDITION
+-- ==========================================
+-- E3: Special Summon & Destroy Logic
+-- ==========================================
 function s.ctfilter(c)
-	return c:IsType(TYPE_COUNTER) and c:IsType(TYPE_TRAP)
+	return c:IsType(TYPE_TRAP) and c:IsType(TYPE_COUNTER)
 end
 
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return eg:IsExists(s.ctfilter,1,nil)
+	return not Duel.IsDamageStep() and eg:IsExists(s.ctfilter,1,nil)
 end
 
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	if chk==0 then
-		return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-	end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
-
+	Duel.SetPossibleOperationInfo(0,CATEGORY_DESTROY,nil,1,0,LOCATION_ONFIELD)
 end
 
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 then
-	if Duel.IsExistingMatchingCard(Card.IsDestructable,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil)
-	and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectMatchingCard(tp,Card.IsDestructable,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
-	if #g>0 then
-	Duel.Destroy(g,REASON_EFFECT)
-	end
-	end
+	if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 then
+		-- Optional destruction check
+		local dg=Duel.GetMatchingGroup(nil,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
+		if #dg>0 and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+			Duel.BreakEffect()
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+			local sg=dg:Select(tp,1,1,nil)
+			Duel.HintSelection(sg)
+			Duel.Destroy(sg,REASON_EFFECT)
+		end
 	end
 end
