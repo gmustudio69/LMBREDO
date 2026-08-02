bdd44e07f26beb8620bc1bb4e833adc06b70c916
Nebby <gmustudio69@gmail.com>
2026-08-02 22:04:24 +0700

    Update c220509.lua

--- script/c220509.lua
+++ script/c220509.lua
@@ -1,31 +1,18 @@
 --Hana - The Wind Spirit
 local s,id=GetID()
 function s.initial_effect(c)
-	--Special Summon + Link Summon
+	--Special Summon this card from your hand
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e1:SetType(EFFECT_TYPE_QUICK_O)
-	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetRange(LOCATION_HAND)
 	e1:SetCountLimit(1,id)
 	e1:SetCondition(s.spcon)
+	e1:SetCost(s.spcost)
 	e1:SetTarget(s.sptg)
 	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
-
-	--Search or send to GY
-	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_TOGRAVE)
-	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e2:SetCode(EVENT_BE_MATERIAL)
-	e2:SetProperty(EFFECT_FLAG_DELAY)
-	e2:SetCountLimit(1,{id,1})
-	e2:SetCondition(s.thcon)
-	e2:SetTarget(s.thtg)
-	e2:SetOperation(s.thop)
-	c:RegisterEffect(e2)
 end
 s.listed_names={220450,id}
 -- Check if you control "<Limit Breaker> Kazari"
@@ -36,37 +23,30 @@ function s.spcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil)
 end
 
--- Special Summon target
+function s.spcostfilter(c)
+	return (c:IsCode(220450) or c:ListsCode(220450)) and c:IsAbleToGraveAsCost()
+end
+function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.IsExistingMatchingCard(s.spcostfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,s.spcostfilter,tp,LOCATION_DECK,0,1,1,nil)
+	Duel.SendtoGrave(g,REASON_COST)
+end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,tp,0)
 end
-
--- Filter for WIND Warrior Link Monsters
-function s.linkfilter(c,e,tp)
-	return c:IsRace(RACE_WARRIOR)
-		and c:IsType(TYPE_LINK)
-		and c:IsLinkSummonable(nil,nil)
-end
-
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 then
-		Duel.BreakEffect()
-		-- Link Summon
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local g=Duel.GetMatchingGroup(s.linkfilter,tp,LOCATION_EXTRA,0,nil,e,tp)
-		if #g>0 then
-			local lc=g:Select(tp,1,1,nil):GetFirst()
-			Duel.LinkSummon(tp,lc,c)
-		end
+	if c:IsRelateToEffect(e) and Duel.SpecialSummonStep(c,0,tp,tp,false,false,POS_FACEUP) then
+		return true
 	end
+	Duel.SpecialSummonComplete()
 end
 
--- Trigger when used as Link Material
-function s.thcon(e,tp,eg,ep,ev,re,r,rp)
-	return r==REASON_LINK
-end
 
 function s.thfilter(c)
 return (c:IsType(TYPE_SPIRIT) or c:IsCode(220450)) and c:IsAbleToHand()
