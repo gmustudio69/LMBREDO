0a80371e7c426dab9817631b32512c67c1688ed1
Nebby <gmustudio69@gmail.com>
2026-08-03 13:44:36 +0700

    Emi

--- script/c220531.lua
+++ script/c220531.lua
@@ -1,72 +1,87 @@
---Nocturne Gear - Secret hideout
+--Emi - The Shadow Beast
 local s,id=GetID()
 function s.initial_effect(c)
-	--Activate Field Spell
-	local e0=Effect.CreateEffect(c)
-	e0:SetType(EFFECT_TYPE_ACTIVATE)
-	c:RegisterEffect(e0)
-
-	--Extra Normal Summon (1 extra Psychic Normal Summon per turn)
+	-- 1. Hand Trigger: Special Summon when taking effect damage & inflict double damage
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetRange(LOCATION_FZONE)
-	e1:SetCode(EFFECT_EXTRA_SUMMON_OPTIONAL)
-	e1:SetTargetRange(LOCATION_HAND,0)
-	e1:SetTarget(aux.TargetBoolFunction(Card.IsRace,RACE_PSYCHIC))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_DAMAGE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_DAMAGE)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.spcon)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
 
-	--Banish 1 Psychic to Special Summon 1 "Nocturne Gear" with a different Level
+	-- 2. On Normal/Special Summon: Search "Shadow Beast" or Level 7 Warrior monster
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_REMOVE)
-	e2:SetType(EFFECT_TYPE_IGNITION)
-	e2:SetRange(LOCATION_FZONE)
-	e2:SetCountLimit(1,id)
-	e2:SetCost(s.spcost)
-	e2:SetTarget(s.sptg)
-	e2:SetOperation(s.spop)
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetCode(EVENT_SUMMON_SUCCESS)
+	e2:SetCountLimit(1,id+100)
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
 	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e3)
 end
 
--- Cost Filter: Must be a Psychic monster on field or GY that is able to be banished
-function s.costfilter(c,e,tp)
-	local lv=c:GetLevel()
-	return c:IsRace(RACE_PSYCHIC) and c:IsAbleToRemoveAsCost() and lv>0
-		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK|LOCATION_GRAVE|LOCATION_REMOVED,0,1,c,e,tp,lv)
-end
+-- ==========================================
+-- Definitions & Setcodes
+-- ==========================================
+local SET_SHADOW_BEAST = 0x98b -- Replace with your actual "Shadow Beast" setcode
 
--- Target Filter: "Nocturne Gear" monster with a different Level than the banished cost card
-function s.spfilter(c,e,tp,lv)
-	return c:IsSetCard(0x999) -- Replace 0x999 with your actual "Nocturne Gear" archetype ID
-		and c:IsType(TYPE_MONSTER)
-		and c:HasLevel()
-		and not c:IsLevel(lv)
-		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-		and (not c:IsLocation(LOCATION_REMOVED) or c:IsFaceup())
-end
-
--- 1. Cost & Target Logic
-function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_MZONE|LOCATION_GRAVE,0,1,nil,e,tp) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g=Duel.SelectMatchingCard(tp,s.costfilter,tp,LOCATION_MZONE|LOCATION_GRAVE,0,1,1,nil,e,tp)
-	e:SetLabel(g:GetFirst():GetLevel())
-	Duel.Remove(g,POS_FACEUP,REASON_COST)
+-- ==========================================
+-- E1: Hand Special Summon & Burn Logic
+-- ==========================================
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	-- Trigger if player (tp) took effect damage, except during the Damage Step
+	return ep==tp and (r&REASON_EFFECT)~=0 and not Duel.IsDamageStep()
 end
 
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end -- Cost already verifies target availability
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK|LOCATION_GRAVE|LOCATION_REMOVED)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	
+	-- Pass the damage taken to operation via label
+	e:SetLabel(ev)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,ev*2)
 end
 
--- 2. Special Summon Operation
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
-	local lv=e:GetLabel()
-	if not lv then return end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK|LOCATION_GRAVE|LOCATION_REMOVED,0,1,1,nil,e,tp,lv)
+	local c=e:GetHandler()
+	local dam=e:GetLabel()
+	if c:IsRelateToEffect(e) and Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)>0 then
+		Duel.BreakEffect()
+		Duel.Damage(1-tp,dam*2,REASON_EFFECT)
+	end
+end
+
+-- ==========================================
+-- E2: Search Logic
+-- ==========================================
+function s.thfilter(c)
+	return (c:IsSetCard(SET_SHADOW_BEAST) or (c:IsRace(RACE_WARRIOR) and c:IsLevel(7)))
+		and c:IsType(TYPE_MONSTER) and not c:IsCode(id) and c:IsAbleToHand()
+end
+
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
 	if #g>0 then
-		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
 	end
 end
